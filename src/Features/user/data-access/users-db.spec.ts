19400184796fbbe25d7e2c19c05e90a0c7b53c50
mrboparai <karanvirboparai123@gmail.com>
2022-08-24 11:08:17 -0400

    added test for finding userId

--- src/Features/user/data-access/users-db.spec.ts
+++ src/Features/user/data-access/users-db.spec.ts
@@ -2,20 +2,18 @@ import UsersDb from ".";
 import makeFakerComment from "../../../../__test__/fixures/user";
 
 describe("Users DB", () => {
+    beforeEach(() => {
+        UsersDb;
+    });
     it("Insert a user", async () => {
         const user = await makeFakerComment();
         const insertUser = await UsersDb.insert({ data: user });
         expect(insertUser?.username).toBe(user.username);
     });
-    it.skip("Find user by Id 1", async () => {
-        const user = await UsersDb.findById({ id: "1" });
-        expect(user).toMatchObject({
-            userid: "1",
-            username: "johnb",
-            email: "gsfs",
-            password: "123ads",
-            status: "online",
-            refreshtoken: "123dasd",
-        });
+    it("Find user by Id", async () => {
+        const fakeUser = await makeFakerComment();
+        const insertedUser = await UsersDb.insert({ data: fakeUser });
+        const user = await UsersDb.findById({ id: fakeUser.userId });
+        expect(user).toEqual(fakeUser);
     });
 });
