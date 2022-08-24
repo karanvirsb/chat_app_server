c441be907339cbcb0c5b39c20d3914c9d9a91154
mrboparai <karanvirboparai123@gmail.com>
2022-08-24 18:45:37 -0400

    added changes according to types

--- src/Features/user/data-access/users-db.spec.ts
+++ src/Features/user/data-access/users-db.spec.ts
@@ -1,37 +1,47 @@
-import makeDb, { clearDb } from "../../../../__test__/fixures/db";
+import makeDb, { clearDb, closeDb } from "../../../../__test__/fixures/db";
 import makeUsersDb, { IMakeUsersDb } from "./users-db";
 import makeFakerComment from "../../../../__test__/fixures/user";
 
 describe("Users DB", () => {
     let UsersDb: IMakeUsersDb["returnType"];
 
-    beforeEach(async () => {
+    beforeAll(async () => {
         UsersDb = makeUsersDb({ makeDb });
         await clearDb();
     });
+
     afterAll(async () => {
         await clearDb();
+        await closeDb();
     });
 
-    it.skip("Insert a user", async () => {
+    it("Insert a user", async () => {
         // successful
         const user = await makeFakerComment();
-        const insertUser = await UsersDb.insert({ data: user });
-        expect(insertUser?.username).toBe(user.username);
+        const resp = await UsersDb.insert({ data: user });
+
+        if (resp.success) {
+            console.log(resp);
+            expect(resp?.data?.username).toBe(user.username);
+        }
     });
-    it.skip("Find user by Id", async () => {
+    it("Find user by Id", async () => {
         //successful
         const fakeUser = await makeFakerComment();
         const insertedUser = await UsersDb.insert({ data: fakeUser });
-        const user = await UsersDb.findById({ id: fakeUser.userId });
-        expect(user?.username).toBe(fakeUser.username);
+        const resp = await UsersDb.findById({ id: fakeUser.userId });
+        if (resp.success) {
+            expect(resp.data?.username).toBe(fakeUser.username);
+        }
     });
 
-    it.skip("Find user by username", async () => {
+    it("Find user by username", async () => {
         // success
         const fakeUser = await makeFakerComment();
         const insertedUser = await UsersDb.insert({ data: fakeUser });
-        const user = await UsersDb.findByUsername(fakeUser.username);
-        expect(user?.username).toBe(fakeUser.username);
+        const resp = await UsersDb.findByUsername(fakeUser.username);
+        if (resp.success) {
+            expect(resp.data?.username).toBe(fakeUser.username);
+        }
     });
 });
