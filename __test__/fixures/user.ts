d2da221d99a00bfbe3841fbd9b226a5cb9b3cb4a
mrboparai <karanvirboparai123@gmail.com>
2022-08-24 10:49:44 -0400

    added async

--- __test__/fixures/user.ts
+++ __test__/fixures/user.ts
@@ -1,13 +1,14 @@
 import { faker } from "@faker-js/faker";
 import bcrypt from "bcrypt";
 import { nanoid } from "nanoid";
+import { IUser } from "../../src/Features/user/user";
 
-export default function makeFakeUser() {
+export default async function makeFakeUser(): Promise<IUser> {
     return {
         userId: nanoid(),
         username: faker.name.firstName() + faker.color.human(),
         email: `${faker.name.fullName()}@gmail.com`,
-        password: bcrypt.hash(faker.random.word(), 10),
+        password: await bcrypt.hash(faker.random.word(), 10),
         status: "online",
         refreshToken: faker.random.alphaNumeric(256),
     };
