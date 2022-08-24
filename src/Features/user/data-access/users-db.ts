1e9d9e588ebd129e1ea5267df51ed241baaaeefc
mrboparai <karanvirboparai123@gmail.com>
2022-08-24 18:44:10 -0400

    fixed insert issues

--- src/Features/user/data-access/users-db.ts
+++ src/Features/user/data-access/users-db.ts
@@ -8,7 +8,7 @@ type props = {
 type returningData = {
     type: Promise<{
         success: boolean;
-        data: { user: IUser } | undefined;
+        data: IUser | undefined;
         error: string;
     }>;
 };
@@ -39,7 +39,7 @@ export default function makeUsersDb({ makeDb }: props) {
         try {
             const query = `SELECT * FROM userT WHERE userId = '${id}'`;
             const res = await db.query(query);
-            return { success: true, data: { user: res.rows[0] }, error: "" };
+            return { success: true, data: await res.rows[0], error: "" };
         } catch (error: any) {
             console.log(error);
             return { success: true, data: undefined, error: error };
@@ -52,7 +52,7 @@ export default function makeUsersDb({ makeDb }: props) {
         try {
             const query = `SELECT * FROM userT WHERE username = '${username}'`;
             const res = await db.query(query);
-            return { success: true, data: { user: res.rows[0] }, error: "" };
+            return { success: true, data: res.rows[0], error: "" };
         } catch (err: any) {
             return { success: true, data: undefined, error: err };
         } finally {
@@ -68,7 +68,7 @@ export default function makeUsersDb({ makeDb }: props) {
                 "INSERT INTO userT VALUES($1, $2, $3, $4, $5, $6) RETURNING *";
 
             const foundUser = await findByUsername(data.username);
-            if (foundUser) {
+            if (foundUser.data !== undefined) {
                 return {
                     success: true,
                     data: undefined,
@@ -84,7 +84,7 @@ export default function makeUsersDb({ makeDb }: props) {
                 data.refreshToken,
             ]);
             const user: IUser = res.rows[0];
-            return { success: true, data: { user }, error: "" };
+            return { success: true, data: user, error: "" };
         } catch (err: any) {
             console.log(err);
             db.release();
