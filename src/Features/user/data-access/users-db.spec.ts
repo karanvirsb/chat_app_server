50bdc9598b19cce2a802e3a0c0fdd7692ebe0faa
mrboparai <karanvirboparai123@gmail.com>
2022-08-24 10:50:11 -0400

    added new test

--- src/Features/user/data-access/users-db.spec.ts
+++ src/Features/user/data-access/users-db.spec.ts
@@ -1,7 +1,13 @@
 import UsersDb from ".";
+import makeFakerComment from "../../../../__test__/fixures/user";
 
 describe("Users DB", () => {
-    it("Find user by Id 1", async () => {
+    it("Insert a user", async () => {
+        const user = await makeFakerComment();
+        const insertUser = await UsersDb.insert({ data: user });
+        expect(insertUser?.username).toBe(user.username);
+    });
+    it.skip("Find user by Id 1", async () => {
         const user = await UsersDb.findById({ id: "1" });
         expect(user).toMatchObject({
             userid: "1",
