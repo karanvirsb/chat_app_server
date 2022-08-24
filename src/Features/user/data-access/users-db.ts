b6c9e8d6ec2f3b8b54c124fb3469506c7ecf5d4c
mrboparai <karanvirboparai123@gmail.com>
2022-08-24 14:35:04 -0400

    added a return type to better suit errors

--- src/Features/user/data-access/users-db.ts
+++ src/Features/user/data-access/users-db.ts
@@ -5,14 +5,22 @@ type props = {
     makeDb: IUserDb["makeDb"];
 };
 
+type returningData = {
+    type: Promise<{
+        success: boolean;
+        data: { user: IUser } | undefined;
+        error: string;
+    }>;
+};
+
 export interface IMakeUsersDb {
     returnType: Readonly<{
-        findById: ({ id }: { id: string }) => Promise<IUser | undefined>;
-        findByUsername: (username: string) => Promise<IUser | undefined>;
+        findById: ({ id }: { id: string }) => returningData["type"];
+        findByUsername: (username: string) => returningData["type"];
         findByEmail: () => Promise<void>;
         update: () => Promise<void>;
         remove: () => Promise<void>;
-        insert: ({ data }: { data: IUser }) => Promise<IUser | undefined>;
+        insert: ({ data }: { data: IUser }) => returningData["type"];
     }>;
 }
 
@@ -26,48 +34,47 @@ export default function makeUsersDb({ makeDb }: props) {
         insert,
     });
 
-    async function findById({
-        id,
-    }: {
-        id: string;
-    }): Promise<IUser | undefined> {
+    async function findById({ id }: { id: string }): returningData["type"] {
         const db = await makeDb();
         try {
             const query = `SELECT * FROM userT WHERE userId = '${id}'`;
             const res = await db.query(query);
-            return res.rows[0];
-        } catch (error) {
+            return { success: true, data: { user: res.rows[0] }, error: "" };
+        } catch (error: any) {
             console.log(error);
+            return { success: true, data: undefined, error: error };
         } finally {
             db.release();
         }
     }
-    async function findByUsername(
-        username: string
-    ): Promise<IUser | undefined> {
+    async function findByUsername(username: string): returningData["type"] {
         const db = await makeDb();
         try {
             const query = `SELECT * FROM userT WHERE username = '${username}'`;
             const res = await db.query(query);
-            return await res.rows[0];
-        } catch (err) {
-            console.log(err);
+            return { success: true, data: { user: res.rows[0] }, error: "" };
+        } catch (err: any) {
+            return { success: true, data: undefined, error: err };
         } finally {
             db.release();
         }
     }
     async function update() {}
     async function remove() {}
-    async function insert({
-        data,
-    }: {
-        data: IUser;
-    }): Promise<IUser | undefined> {
+    async function insert({ data }: { data: IUser }): returningData["type"] {
         const db = await makeDb();
         try {
             const query =
                 "INSERT INTO userT VALUES($1, $2, $3, $4, $5, $6) RETURNING *";
-            // TODO find by username
+
+            const foundUser = await findByUsername(data.username);
+            if (foundUser) {
+                return {
+                    success: true,
+                    data: undefined,
+                    error: "User already exists",
+                };
+            }
             const res = await db.query(query, [
                 data.userId,
                 data.username,
@@ -77,10 +84,11 @@ export default function makeUsersDb({ makeDb }: props) {
                 data.refreshToken,
             ]);
             const user: IUser = res.rows[0];
-            return user;
-        } catch (err) {
+            return { success: true, data: { user }, error: "" };
+        } catch (err: any) {
             console.log(err);
             db.release();
+            return { success: true, data: undefined, error: err };
         } finally {
             db.release();
         }
