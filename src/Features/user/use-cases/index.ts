14fbc681bb7114fc6227b8aa4881b19ad3ca66a1
mrboparai <karanvirboparai123@gmail.com>
2022-08-25 19:58:14 -0400

    added delete user

--- src/Features/user/use-cases/index.ts
+++ src/Features/user/use-cases/index.ts
@@ -2,6 +2,7 @@ import makeAddUser from "./addUser";
 import makeEditUser from "./editUser";
 import { moderateName } from "../../../Utilities/moderateText";
 import usersDb from "../data-access";
+import makeDeleteUser from "./deleteUser";
 
 export type handleModerationType = (name: string) => Promise<number | boolean>;
 
@@ -11,11 +12,14 @@ const handleModeration = async (name: string) => {
 
 const addUser = makeAddUser({ usersDb, handleModeration });
 const editUser = makeEditUser({ usersDb, handleModeration });
+const deleteUser = makeDeleteUser({ usersDb });
+
 const userService = Object.freeze({
     addUser,
     editUser,
+    deleteUser,
 });
 
 export default userService;
 
-export { addUser, editUser };
+export { addUser, editUser, deleteUser };
