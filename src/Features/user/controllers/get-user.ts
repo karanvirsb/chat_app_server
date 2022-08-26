ad52b1df935e7a84723c3f704a9fa2e1f551f471
mrboparai <karanvirboparai123@gmail.com>
2022-08-26 10:28:36 -0400

    created a controller

---
+++ src/Features/user/controllers/get-user.ts
@@ -0,0 +1,27 @@
+import { IGetUserUseCase } from "../use-cases/getUser";
+
+export default function makeGetUser({ getUser }: IGetUserUseCase) {
+    return async function getUserController(httpRequest: any) {
+        const headers = {
+            "Content-Type": "application/json",
+        };
+
+        try {
+            const user = await getUser(httpRequest.params.username);
+            return {
+                headers,
+                statusCode: 200,
+                body: user,
+            };
+        } catch (error: any) {
+            console.log(error);
+            return {
+                headers,
+                statusCode: 400,
+                body: {
+                    error: error.message,
+                },
+            };
+        }
+    };
+}
