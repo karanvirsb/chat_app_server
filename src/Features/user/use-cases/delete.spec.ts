66c1a5a3c27967e48f9ad5a2c12137df7ae48282
mrboparai <karanvirboparai123@gmail.com>
2022-08-25 20:19:53 -0400

    added deleteUser tests

---
+++ src/Features/user/use-cases/delete.spec.ts
@@ -0,0 +1,33 @@
+import makeDeleteUser from "./deleteUser";
+import makeDb, { clearDb } from "../../../../__test__/fixures/db";
+import makeUsersDb from "../data-access/users-db";
+import makeFakeUser from "../../../../__test__/fixures/user";
+
+describe("Delete use case", () => {
+    let usersDb = makeUsersDb({ makeDb });
+    let deleteUser = makeDeleteUser({ usersDb });
+
+    afterAll(() => {
+        clearDb();
+    });
+
+    it("Deleted user successfully", async () => {
+        const user = await makeFakeUser();
+        const insertedUser = await usersDb.insert({ data: user });
+        const deletedUser = await deleteUser(user.userId);
+        console.log(deletedUser);
+        if (deletedUser.success) {
+            expect(deletedUser.data?.userId).toBe(user.userId);
+        }
+    });
+
+    it.skip("User does not exist", async () => {
+        const deletedUser = await deleteUser("1");
+
+        if (deletedUser.success && deletedUser.data === undefined) {
+            expect(deletedUser.error).toBe(
+                "Could not find any user with that id"
+            );
+        }
+    });
+});
