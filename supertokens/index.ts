4f67ff9f8aec2cd2b8ad70b42f5eb67187e63805
mrboparai <karanvirboparai123@gmail.com>
2022-08-26 13:47:14 -0400

    added superToken initialization

---
+++ supertokens/index.ts
@@ -0,0 +1,26 @@
+import supertokens from "supertokens-node";
+import Session from "supertokens-node/recipe/session";
+import EmailPassword from "supertokens-node/recipe/emailpassword";
+
+supertokens.init({
+    framework: "express",
+    supertokens: {
+        // try.supertokens.com is for demo purposes. Replace this with the address of your core instance (sign up on supertokens.com), or self host a core.
+        connectionURI: "http://localhost:8001",
+        // apiKey: "IF YOU HAVE AN API KEY FOR THE CORE, ADD IT HERE",
+    },
+    appInfo: {
+        // learn more about this on https://supertokens.com/docs/session/appinfo
+        appName: "chat app",
+        apiDomain: "http://localhost:8000",
+        websiteDomain: "http://localhost:3000",
+        apiBasePath: "/auth",
+        websiteBasePath: "/auth",
+    },
+    recipeList: [
+        EmailPassword.init(), // initializes signin / sign up features
+        Session.init(), // initializes session features
+    ],
+});
+
+export default supertokens;
