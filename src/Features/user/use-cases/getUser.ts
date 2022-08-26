7b26363cf4962a184f2089c61c67dfdea0c3b38c
mrboparai <karanvirboparai123@gmail.com>
2022-08-25 20:23:10 -0400

    created a getUser

---
+++ src/Features/user/use-cases/getUser.ts
@@ -0,0 +1,21 @@
+import { IUser } from "../user";
+import { IMakeUsersDb } from "../data-access/users-db";
+
+type props = {
+    usersDb: IMakeUsersDb["returnType"];
+};
+
+type returnData = Promise<{
+    success: boolean;
+    data: IUser | undefined;
+    error: string;
+}>;
+
+export default function makeGetUser({ usersDb }: props) {
+    return async function getUser(username: string): returnData {
+        if (!username) {
+            throw new Error("Username must exist");
+        }
+        return await usersDb.findByUsername(username);
+    };
+}
