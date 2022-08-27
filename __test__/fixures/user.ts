dc6c430e0499ecf8a6937fcbffde3416b456b329
mrboparai <karanvirboparai123@gmail.com>
2022-08-27 12:20:01 -0400

    removed password adn refreshtoken

--- __test__/fixures/user.ts
+++ __test__/fixures/user.ts
@@ -1,5 +1,4 @@
 import { faker } from "@faker-js/faker";
-import bcrypt from "bcrypt";
 import { nanoid } from "nanoid";
 import { IUser } from "../../src/Features/user/user";
 
@@ -8,8 +7,6 @@ export default async function makeFakeUser(): Promise<IUser> {
         userId: nanoid(),
         username: faker.name.firstName() + faker.color.human(),
         email: `${faker.name.fullName()}@gmail.com`,
-        password: await bcrypt.hash(faker.random.word(), 10),
         status: "online",
-        refreshToken: faker.random.alphaNumeric(256),
     };
 }
