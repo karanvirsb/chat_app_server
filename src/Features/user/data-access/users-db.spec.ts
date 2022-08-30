3e33f7eb08fefd60a64e391f59d80eabd1ade0f7
mrboparai <karanvirboparai123@gmail.com>
2022-08-30 11:18:45 -0400

    changed tests

--- src/Features/user/data-access/users-db.spec.ts
+++ src/Features/user/data-access/users-db.spec.ts
@@ -2,11 +2,15 @@ import makeDb, { clearDb, closeDb } from "../../../../__test__/fixures/db";
 import makeUsersDb, { IMakeUsersDb } from "./users-db";
 import makeFakerComment from "../../../../__test__/fixures/user";
 
-describe.skip("Users DB", () => {
+describe("Users DB", () => {
     let UsersDb: IMakeUsersDb["returnType"];
 
     beforeAll(async () => {
         UsersDb = makeUsersDb({ makeDb });
+        await clearDb();
+    });
+
+    afterEach(async () => {
         await clearDb();
     });
 
@@ -15,7 +19,7 @@ describe.skip("Users DB", () => {
         await closeDb();
     });
 
-    it.skip("Insert a user", async () => {
+    it("Insert a user", async () => {
         // successful
         const user = await makeFakerComment();
         const resp = await UsersDb.insert({ data: user });
@@ -24,7 +28,7 @@ describe.skip("Users DB", () => {
             expect(resp?.data?.username).toBe(user.username);
         }
     });
-    it.skip("Find user by Id", async () => {
+    it("Find user by Id", async () => {
         //successful
         const fakeUser = await makeFakerComment();
         const insertedUser = await UsersDb.insert({ data: fakeUser });
@@ -34,26 +38,17 @@ describe.skip("Users DB", () => {
         }
     });
 
-    it.skip("Find user by username", async () => {
+    it("Find user by username", async () => {
         // success
         const fakeUser = await makeFakerComment();
         const insertedUser = await UsersDb.insert({ data: fakeUser });
         const resp = await UsersDb.findByUsername(fakeUser.username);
         if (resp.success) {
-            expect(resp.data?.email).toBe(fakeUser.email);
-        }
-    });
-
-    it.skip("Find user by email", async () => {
-        const fakeUser = await makeFakerComment();
-        const insertedUser = await UsersDb.insert({ data: fakeUser });
-        const resp = await UsersDb.findByEmail(fakeUser.email);
-        if (resp.success) {
             expect(resp.data?.username).toBe(fakeUser.username);
         }
     });
 
-    it.skip("Remove user by Id", async () => {
+    it("Remove user by Id", async () => {
         const fakeUser = await makeFakerComment();
         const insertedUser = await UsersDb.insert({ data: fakeUser });
         const resp = await UsersDb.remove(fakeUser.userId);
@@ -62,15 +57,27 @@ describe.skip("Users DB", () => {
         }
     });
 
-    it.skip("Update user", async () => {
+    it("Update user by username", async () => {
         const fakeUser = await makeFakerComment();
         const insertedUser = await UsersDb.insert({ data: fakeUser });
-        const resp = await UsersDb.update({
+        const resp = await UsersDb.updateByUsername({
+            username: fakeUser.username,
+            updates: {
+                status: "offline",
+            },
+        });
+        if (resp.success) {
+            expect(resp.data?.status).toBe("offline");
+        }
+    });
+
+    it("Update user by user id", async () => {
+        const fakeUser = await makeFakerComment();
+        const insertedUser = await UsersDb.insert({ data: fakeUser });
+        const resp = await UsersDb.updateByUserId({
             userId: fakeUser.userId,
             updates: {
                 username: "johnB",
-                email: "",
-                userId: "",
                 status: "offline",
             },
         });
