ef301bfa606957a502a6e4e5b869b0a0ca65798c
mrboparai <karanvirboparai123@gmail.com>
2022-08-30 13:09:30 -0400

    changed up tests according to new objs

--- src/Features/user/use-cases/getUser.spec.ts
+++ src/Features/user/use-cases/getUser.spec.ts
@@ -3,7 +3,7 @@ import makeDb, { clearDb } from "../../../../__test__/fixures/db";
 import makeUsersDb from "../data-access/users-db";
 import makeFakeUser from "../../../../__test__/fixures/user";
 
-describe.skip("Delete use case", () => {
+describe.skip("Get use case", () => {
     jest.setTimeout(50000);
     let usersDb = makeUsersDb({ makeDb });
     let getUser = makeGetUser({ usersDb });
@@ -12,24 +12,26 @@ describe.skip("Delete use case", () => {
         clearDb();
     });
 
-    it("Email was not supplied", async () => {
-        expect(getUser("")).rejects.toThrow("Email must be passed through");
+    it("UserId was not supplied", async () => {
+        expect(getUser("")).rejects.toThrow("UserId must be passed through");
     });
 
     it("Get user who exists", async () => {
         const user = await makeFakeUser();
         await usersDb.insert({ data: user });
 
-        const foundUser = await getUser(user.email);
+        const foundUser = await getUser(user.userId);
         if (foundUser.success) {
-            expect(foundUser.data?.userId).toBe(user.userId);
+            expect(foundUser.data?.userId.trim()).toBe(user.userId.trim());
         }
     });
 
     it("Get user who does not exist", async () => {
         const foundUser = await getUser("1");
         if (foundUser.success && foundUser.data === undefined) {
-            expect(foundUser.error).toBe("Could not find user with that email");
+            expect(foundUser.error).toBe(
+                "Could not find any user with that id"
+            );
         }
     });
 });
