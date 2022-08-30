c1c961778a3dd1046ac8e9c58307d37f03c9c62c
mrboparai <karanvirboparai123@gmail.com>
2022-08-30 10:15:03 -0400

    removed id not needed

--- src/Features/user/index.ts
+++ src/Features/user/index.ts
@@ -1,8 +1,8 @@
-import Id from "../../Utilities/id";
+// import Id from "../../Utilities/id";
 import buildUser from "./user";
 import sanitizeHtml from "sanitize-html";
 
-export default buildUser({ Id, sanitizeText });
+export default buildUser({ sanitizeText });
 
 function sanitizeText(text: string) {
     return sanitizeHtml(text);
