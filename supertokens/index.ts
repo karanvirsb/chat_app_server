812cb5bd542d352b6577f2cf4b432818cfdc1369
mrboparai <karanvirboparai123@gmail.com>
2022-09-22 09:52:53 -0400

    added process env varaibles

--- supertokens/index.ts
+++ supertokens/index.ts
@@ -24,14 +24,14 @@ supertokens.init({
     framework: "express",
     supertokens: {
         // try.supertokens.com is for demo purposes. Replace this with the address of your core instance (sign up on supertokens.com), or self host a core.
-        connectionURI: "http://localhost:8001",
+        connectionURI: process.env.SUPERTOKENS_CONNECTION_URI!,
         // apiKey: "IF YOU HAVE AN API KEY FOR THE CORE, ADD IT HERE",
     },
     appInfo: {
         // learn more about this on https://supertokens.com/docs/session/appinfo
         appName: "chat app",
-        apiDomain: "http://localhost:8000",
-        websiteDomain: "http://localhost:3000",
+        apiDomain: process.env.API_DOMAIN!,
+        websiteDomain: process.env.WEBSITE_DOMAIN!,
         apiBasePath: "/auth",
         websiteBasePath: "/auth",
     },
