b953d2cddc65c79dc58e2e413bfbf220ae921e90
mrboparai <karanvirboparai123@gmail.com>
2022-09-23 11:35:49 -0400

    created template for get user tests

---
+++ src/Features/user/controllers/get-user.spec.ts
@@ -0,0 +1,6 @@
+import makeGetUser from "./get-user";
+import makeFakeUser from "../../../../__test__/fixures/user";
+
+describe("Get user controller", () => {
+    it("successfully retrieve user", () => {});
+});
