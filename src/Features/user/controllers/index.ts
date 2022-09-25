717e6dd0ae3da4251d4df8d3ecf9e56755d460af
mrboparai <karanvirboparai123@gmail.com>
2022-09-25 10:41:52 -0400

    added post user

--- src/Features/user/controllers/index.ts
+++ src/Features/user/controllers/index.ts
@@ -1,11 +1,14 @@
-import { getUser } from "../use-cases";
+import { getUser, addUser } from "../use-cases";
 import makeGetUser from "./get-user";
+import makePostUser from "./post-user";
 
 const getAnUser = makeGetUser({ getUser });
+const addAnUser = makePostUser({ addUser });
 
 const commentController = Object.freeze({
     getAnUser,
+    addAnUser,
 });
 export default commentController;
 
-export { getAnUser };
+export { getAnUser, addAnUser };
