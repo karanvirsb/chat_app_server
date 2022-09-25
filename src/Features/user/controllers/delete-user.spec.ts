df781f0dcf8f845623bd1a301114bebdfffb114e
mrboparai <karanvirboparai123@gmail.com>
2022-09-25 11:49:26 -0400

    added deleting test

---
+++ src/Features/user/controllers/delete-user.spec.ts
@@ -0,0 +1,22 @@
+import { deleteAnUser } from ".";
+
+describe("delete user controller", () => {
+    it("deleting user", async () => {
+        const deletedUser = await deleteAnUser({
+            body: {
+                id: "ce3735e4-b3de-48d4-853e-758c06b1a935",
+            },
+            headers: {},
+            ip: "string",
+            method: "GET",
+            params: {},
+            path: "",
+            query: {},
+        });
+
+        expect(deletedUser.body.data.userId).toBe(
+            "ce3735e4-b3de-48d4-853e-758c06b1a935"
+        );
+        console.log(deletedUser);
+    });
+});
