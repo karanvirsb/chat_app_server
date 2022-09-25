1bac1ef882564d75be100f29e15f404c40fb473f
mrboparai <karanvirboparai123@gmail.com>
2022-09-25 10:27:50 -0400

    added a test for getting user

--- src/Features/user/controllers/get-user.spec.ts
+++ src/Features/user/controllers/get-user.spec.ts
@@ -1,6 +1,18 @@
-import makeGetUser from "./get-user";
+import { getAnUser } from ".";
 import makeFakeUser from "../../../../__test__/fixures/user";
 
 describe("Get user controller", () => {
-    it("successfully retrieve user", () => {});
+    it("successfully retrieve user", async () => {
+        const user = await getAnUser({
+            body: {},
+            headers: {},
+            ip: "string",
+            method: "GET",
+            params: { id: "5c0fc896-1af1-4c26-b917-550ac5eefa9e" },
+            path: "",
+            query: {},
+        });
+        console.log(user);
+        expect(user.body.data.username).toBe("tester");
+    });
 });
