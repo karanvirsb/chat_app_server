1d76a72ee45d39feebe009d5ff0e8ebddbbf81c3
mrboparai <karanvirboparai123@gmail.com>
2022-09-25 10:45:44 -0400

    aded delete to the controller

--- src/Features/user/controllers/index.ts
+++ src/Features/user/controllers/index.ts
@@ -1,14 +1,17 @@
-import { getUser, addUser } from "../use-cases";
+import { getUser, addUser, deleteUser } from "../use-cases";
+import makeDeleteUser from "./delete-user";
 import makeGetUser from "./get-user";
 import makePostUser from "./post-user";
 
 const getAnUser = makeGetUser({ getUser });
 const addAnUser = makePostUser({ addUser });
+const deleteAnUser = makeDeleteUser({ deleteUser });
 
 const commentController = Object.freeze({
     getAnUser,
     addAnUser,
+    deleteAnUser,
 });
 export default commentController;
 
-export { getAnUser, addAnUser };
+export { getAnUser, addAnUser, deleteAnUser };
