2295cea7d3003cb9cf3e80d1a976f4a50719f835
mrboparai <karanvirboparai123@gmail.com>
2022-09-25 10:59:35 -0400

    added edit user

--- src/Features/user/controllers/index.ts
+++ src/Features/user/controllers/index.ts
@@ -1,17 +1,20 @@
-import { getUser, addUser, deleteUser } from "../use-cases";
+import { getUser, addUser, deleteUser, editUser } from "../use-cases";
 import makeDeleteUser from "./delete-user";
+import makeEditUser from "./edit-user";
 import makeGetUser from "./get-user";
 import makePostUser from "./post-user";
 
 const getAnUser = makeGetUser({ getUser });
 const addAnUser = makePostUser({ addUser });
 const deleteAnUser = makeDeleteUser({ deleteUser });
+const editAnUser = makeEditUser({ editUser });
 
 const userController = Object.freeze({
     getAnUser,
     addAnUser,
     deleteAnUser,
+    editAnUser,
 });
 export default userController;
 
-export { getAnUser, addAnUser, deleteAnUser };
+export { getAnUser, addAnUser, deleteAnUser, editAnUser };
