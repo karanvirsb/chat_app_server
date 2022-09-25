afb7d9fb0bcb774ce9104793d500c1b488bd69f7
mrboparai <karanvirboparai123@gmail.com>
2022-09-25 11:22:10 -0400

    added test for editting user

---
+++ src/Features/user/controllers/edit-user.spec.ts
@@ -0,0 +1,21 @@
+import { editAnUser } from ".";
+
+describe.skip("", () => {
+    it("", async () => {
+        const edittedUser = await editAnUser({
+            body: {
+                id: "312c0878-04c3-4585-835e-c66900ccc7a1",
+                updates: { status: "offline" },
+            },
+            headers: {},
+            ip: "string",
+            method: "GET",
+            params: {},
+            path: "",
+            query: {},
+        });
+
+        expect(edittedUser.body.data.status).toBe("offline");
+        console.log(edittedUser);
+    });
+});
