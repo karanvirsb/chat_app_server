04bdd9399a5a09ae09936a0a52a286c4d217a458
mrboparai <karanvirboparai123@gmail.com>
2022-09-26 08:53:13 -0400

    added invite code and sanitize text dependencies

--- src/Features/group/index.ts
+++ src/Features/group/index.ts
@@ -1,4 +1,10 @@
 import buildGroup from "./group";
 import Id from "../../Utilities/id";
+import inviteCodeGenerator from "../../Utilities/inviteCodeGenerator";
+import sanitizeHtml from "sanitize-html";
 
-export default buildGroup({ Id });
+export default buildGroup({ Id, inviteCodeGenerator, sanitizeText });
+
+function sanitizeText(text: string) {
+    return sanitizeHtml(text);
+}
