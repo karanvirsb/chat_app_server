3bc2e2b2126ba19c4905af9b413bd21a2d8d4035
mrboparai <karanvirboparai123@gmail.com>
2022-09-26 13:46:22 -0400

    created a table initializer for group

---
+++ src/Features/group/db/index.ts
@@ -0,0 +1,31 @@
+import { makeDb } from "../data-access";
+import dotenv from "dotenv";
+dotenv.config();
+
+export default async function setupGroupDb() {
+    console.log("setting up group database...");
+    // database will be created if it doesnt exist already
+
+    const db = await makeDb();
+    const result = await db.query(`
+    CREATE TABLE IF NOT EXISTS groupt (
+      "groupId" VARCHAR(50) PRIMARY KEY NOT NULL UNIQUE, 
+      "groupName" VARCHAR(50) NOT NULL, 
+      "inviteCode" VARCHAR(10) UNIQUE, 
+      channels text[]
+    );
+    
+    CREATE TABLE IF NOT EXISTS "groupUsers" (
+      "gId" VARCHAR(50) REFERENCES groupt("groupId"),
+      "uId" VARCHAR(100) REFERENCES usert("userId"),
+      roles integer[],
+      PRIMARY KEY ("gId", "uId")
+    );
+  `);
+    console.log(
+        "🚀 ~ file: index.ts ~ line 24 ~ setupGroupDb ~ result",
+        result
+    );
+    console.log("Group Database set up complete...");
+    process.exit();
+}
