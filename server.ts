a98738058959626287ac56433c48051db74b629a
mrboparai <karanvirboparai123@gmail.com>
2022-09-26 13:46:29 -0400

    added group setUp

--- server.ts
+++ server.ts
@@ -1,6 +1,9 @@
 const PORT = 8000;
 import app from "./app";
 
+import setUpGroupDb from "./src/Features/group/db";
+
 app.listen(PORT, () => {
     console.log("listening on port", PORT);
+    setUpGroupDb();
 });
