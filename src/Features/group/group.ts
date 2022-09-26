f5b7b2e85576e06558a692515f2c4f5f126a9a9d
mrboparai <karanvirboparai123@gmail.com>
2022-09-26 08:41:37 -0400

    added invite code generator and error checks

--- src/Features/group/group.ts
+++ src/Features/group/group.ts
@@ -1,7 +1,10 @@
 import { IId } from "../../Utilities/id";
+import { IInviteCodeGenerator } from "../../Utilities/inviteCodeGenerator";
 
 type props = {
     Id: IId;
+    sanitizeText: (text: string) => string;
+    inviteCodeGenerator: IInviteCodeGenerator;
 };
 
 export interface IGroup {
@@ -10,12 +13,38 @@ export interface IGroup {
     inviteCode: string;
 }
 
-export default function buildGroup({ Id }: props) {
+export default function buildGroup({
+    Id,
+    sanitizeText,
+    inviteCodeGenerator,
+}: props) {
     return function makeGroup({
         groupName,
         groupId = Id.makeId(),
-        inviteCode,
+        inviteCode = inviteCodeGenerator.makeInviteCode(),
     }: IGroup) {
+        let sanitizedGroupName = sanitizeText(groupName);
+
+        if (sanitizedGroupName.length < 1) {
+            throw new Error(
+                "Group name must cantain valid characters not HTML"
+            );
+        }
+
+        if (sanitizedGroupName.length <= 3 && sanitizedGroupName.length >= 50) {
+            throw new Error(
+                "Group name must be between 3 and 50 characters long"
+            );
+        }
+
+        if (!groupId) {
+            throw new Error("Group requires an Id");
+        }
+
+        if (!inviteCode) {
+            throw new Error("Group requires an invite code");
+        }
+
         return {
             getGroupName: () => groupName,
             getGroupId: () => groupId,
