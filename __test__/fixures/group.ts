fd7d8238670ed7a0f3c471b27e1144736b8261fb
mrboparai <karanvirboparai123@gmail.com>
2022-09-26 08:57:30 -0400

    created a fake group

---
+++ __test__/fixures/group.ts
@@ -0,0 +1,12 @@
+import { faker } from "@faker-js/faker";
+import cuid from "cuid";
+import { IGroup } from "../../src/Features/group/group";
+
+export default async function makeFakeGroup(): Promise<IGroup> {
+    return {
+        groupId: cuid(),
+        groupName: faker.animal + "" + faker.color,
+        inviteCode: cuid.slug(),
+        channels: ["general"],
+    };
+}
