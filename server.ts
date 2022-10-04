eaf3d5b438b3812d17643f6f0db4e50633c2eacf
mrboparai <karanvirboparai123@gmail.com>
2022-10-04 11:11:36 -0400

    added port within string literals

--- server.ts
+++ server.ts
@@ -4,6 +4,6 @@ import app from "./app";
 import setUpGroupDb from "./src/Features/group/db";
 
 app.listen(PORT, () => {
-    console.log("listening on port", PORT);
+    console.log(`listening on port: ${PORT}`);
     setUpGroupDb();
 });
