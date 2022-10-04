14969559fdfc6530419741b97e88af2e103b7648
mrboparai <karanvirboparai123@gmail.com>
2022-10-04 17:41:36 -0400

    created an addGroup use case

---
+++ src/Features/group/use-cases/addGroup.ts
@@ -0,0 +1,62 @@
+import makeGroup from "../index";
+import { handleModerationType } from ".";
+import { IMakeGroupDb } from "../data-access/group-db";
+import { IGroup } from "../group";
+
+type props = {
+    groupDb: IMakeGroupDb["returnType"];
+    handleModeration: handleModerationType;
+};
+
+type returnData = Promise<{
+    success: boolean;
+    data: IGroup | undefined;
+    error: string;
+}>;
+
+export default function makeAddGroup({ groupDb, handleModeration }: props) {
+    return async function addGroup(
+        groupInfo: IGroup,
+        userId: string
+    ): Promise<returnData> {
+        if (!userId) throw new Error("User id needs to be supplied");
+        const group = makeGroup(groupInfo);
+        const foundGroup = await groupDb.findById(group.getGroupId());
+
+        if (foundGroup.success && foundGroup.data !== undefined) {
+            return {
+                success: true,
+                data: undefined,
+                error: "Group already exists",
+            };
+        }
+
+        const moderatedName = await handleModeration(group.getGroupName());
+
+        if (moderatedName) {
+            return {
+                success: false,
+                data: undefined,
+                error: "Group name contains profanity",
+            };
+        }
+
+        if (moderatedName === -1) {
+            return {
+                success: false,
+                data: undefined,
+                error: "Server Error, please try again.",
+            };
+        }
+
+        return await groupDb.createGroup(
+            {
+                groupId: group.getGroupId(),
+                groupName: group.getGroupName(),
+                inviteCode: group.getInviteCode(),
+                channels: group.getChannels(),
+            },
+            userId
+        );
+    };
+}
