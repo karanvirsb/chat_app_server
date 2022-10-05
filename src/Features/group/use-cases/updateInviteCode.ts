9a90f274332ba9f19a6d0e88b2155c24db007322
mrboparai <karanvirboparai123@gmail.com>
2022-10-05 11:41:32 -0400

    created an invite code regenerator use case

---
+++ src/Features/group/use-cases/updateInviteCode.ts
@@ -0,0 +1,34 @@
+import { inviteCodeGeneratorType } from ".";
+import { IMakeGroupDb } from "../data-access/group-db";
+import { IGroup } from "../group";
+
+type props = {
+    groupDb: IMakeGroupDb["returnType"];
+    inviteCodeGenerator: inviteCodeGeneratorType;
+};
+
+type returnData = Promise<{
+    success: boolean;
+    data: IGroup | undefined;
+    error: string;
+}>;
+
+export default function makeUpdateInviteCode({
+    groupDb,
+    inviteCodeGenerator,
+}: props) {
+    return async function updateInviteCode(
+        groupId: string
+    ): Promise<returnData> {
+        if (!groupId) throw new Error("Group Id needs to be supplied");
+
+        let newInviteCode = inviteCodeGenerator.makeInviteCode();
+        let foundGroup = await groupDb.findByInviteCode(newInviteCode);
+        while (foundGroup.data !== undefined) {
+            newInviteCode = inviteCodeGenerator.makeInviteCode();
+            foundGroup = await groupDb.findByInviteCode(newInviteCode);
+        }
+
+        return await groupDb.updateInviteCode(groupId, newInviteCode);
+    };
+}
