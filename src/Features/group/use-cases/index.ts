56a9bbb1305eb6371beb143288d349b349a38671
mrboparai <karanvirboparai123@gmail.com>
2022-10-05 10:38:36 -0400

    added sanitize text and update group name use case

--- src/Features/group/use-cases/index.ts
+++ src/Features/group/use-cases/index.ts
@@ -1,9 +1,11 @@
+import sanitizeHtml from "sanitize-html";
 import { moderateName } from "../../../Utilities/moderateText";
 import groupDb from "../data-access";
 import makeAddGroup from "./addGroup";
 import makeGetGroupById from "./getGroupbyId";
 import makeGetGroupByInviteCode from "./getGroupByInviteCode";
 import makeGetUsersByGroupId from "./getUsersByGroupId";
+import makeUpdateGroupName from "./updateGroupName";
 
 export type handleModerationType = (name: string) => Promise<number | boolean>;
 
@@ -11,18 +13,27 @@ const handleModeration = async (name: string) => {
     return await moderateName(name);
 };
 
+function sanitizeText(text: string) {
+    return sanitizeHtml(text);
+}
 const addGroup = makeAddGroup({ groupDb, handleModeration });
 const getGroupById = makeGetGroupById({ groupDb });
 const getGroupByInviteCode = makeGetGroupByInviteCode({ groupDb });
 const getUsersByGroupId = makeGetUsersByGroupId({ groupDb });
+const updateGroupName = makeUpdateGroupName({
+    groupDb,
+    handleModeration,
+    sanitizeName: sanitizeText,
+});
 
 const userService = Object.freeze({
     addGroup,
     getGroupById,
     getGroupByInviteCode,
     getUsersByGroupId,
+    updateGroupName,
 });
 
 export default userService;
 
-export { addGroup, getGroupById, getGroupByInviteCode };
+export { addGroup, getGroupById, getGroupByInviteCode, updateGroupName };
