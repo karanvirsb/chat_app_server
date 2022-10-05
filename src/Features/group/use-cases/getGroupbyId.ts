a28824dd126e8652fdfdc311e0c8f6e3b91282ae
mrboparai <karanvirboparai123@gmail.com>
2022-10-05 09:24:48 -0400

    created use case for getting group by id

---
+++ src/Features/group/use-cases/getGroupbyId.ts
@@ -0,0 +1,20 @@
+import { IMakeGroupDb } from "../data-access/group-db";
+import { IGroup } from "../group";
+
+type props = {
+    groupDb: IMakeGroupDb["returnType"];
+};
+
+type returnData = Promise<{
+    success: boolean;
+    data: IGroup | undefined;
+    error: string;
+}>;
+
+export default function makeGetGroupById({ groupDb }: props) {
+    return async function getGroupById(groupId: string): Promise<returnData> {
+        if (!groupId) throw new Error("Group Id needs to be supplied");
+
+        return await groupDb.findById(groupId);
+    };
+}
