9644b948cc1333a73460ccc2ed8630588eb6dec6
mrboparai <karanvirboparai123@gmail.com>
2022-10-05 09:27:05 -0400

    added get group by invite code use case

--- src/Features/group/use-cases/index.ts
+++ src/Features/group/use-cases/index.ts
@@ -2,6 +2,7 @@ import { moderateName } from "../../../Utilities/moderateText";
 import groupDb from "../data-access";
 import makeAddGroup from "./addGroup";
 import makeGetGroupById from "./getGroupbyId";
+import makeGetGroupByInviteCode from "./getGroupByInviteCode";
 
 export type handleModerationType = (name: string) => Promise<number | boolean>;
 
@@ -11,12 +12,14 @@ const handleModeration = async (name: string) => {
 
 const addGroup = makeAddGroup({ groupDb, handleModeration });
 const getGroupById = makeGetGroupById({ groupDb });
+const getGroupByInviteCode = makeGetGroupByInviteCode({ groupDb });
 
 const userService = Object.freeze({
     addGroup,
     getGroupById,
+    getGroupByInviteCode,
 });
 
 export default userService;
 
-export { addGroup, getGroupById };
+export { addGroup, getGroupById, getGroupByInviteCode };
