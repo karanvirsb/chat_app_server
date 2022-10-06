0fb4b7e1e0bfde1c41577a7f7fba04423ed4b872
mrboparai <karanvirboparai123@gmail.com>
2022-10-06 11:31:01 -0400

    added delete group controller

--- src/Features/group/controllers/index.ts
+++ src/Features/group/controllers/index.ts
@@ -1,14 +1,19 @@
 import groupService from "../use-cases";
 import makeAddGroupController from "./add-group";
+import makeDeleteGroupController from "./delete-group";
 
 const addGroupController = makeAddGroupController({
     addGroup: groupService.addGroup,
 });
+const deleteGroupController = makeDeleteGroupController({
+    deleteGroup: groupService.deleteGroup,
+});
 
 const groupControllers = Object.freeze({
     addGroupController,
+    deleteGroupController,
 });
 
 export default groupControllers;
 
-export { addGroupController };
+export { addGroupController, deleteGroupController };
