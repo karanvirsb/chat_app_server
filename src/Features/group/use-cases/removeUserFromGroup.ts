f3bcd38f37a9a36c9855d3dbea0cba8202422123
mrboparai <karanvirboparai123@gmail.com>
2022-10-06 09:36:37 -0400

    created a use case for removing user from group

---
+++ src/Features/group/use-cases/removeUserFromGroup.ts
@@ -0,0 +1,23 @@
+import { groupUsers, IMakeGroupDb } from "../data-access/group-db";
+
+type props = {
+    groupDb: IMakeGroupDb["returnType"];
+};
+
+type returnData = Promise<{
+    success: boolean;
+    data: groupUsers | undefined;
+    error: string;
+}>;
+
+export default function makeRemoveUserFromGroup({ groupDb }: props) {
+    return async function removeUserFromGroup(
+        groupId: string,
+        userId: string
+    ): Promise<returnData> {
+        if (!groupId) throw new Error("Group Id needs to be supplied");
+        if (!userId) throw new Error("User Id needs to be supplied");
+
+        return await groupDb.removeUserFromGroup(groupId, userId);
+    };
+}
