4fde0bb18803611cbe5cbf045f3e5d00ad30b3a6
mrboparai <karanvirboparai123@gmail.com>
2022-10-06 11:17:57 -0400

    added an additional return type

--- src/Features/group/controllers/add-group.ts
+++ src/Features/group/controllers/add-group.ts
@@ -2,12 +2,21 @@ import {
     IHttpRequest,
     httpResponseType,
 } from "../../../express-callback/index";
+import { IGroup } from "../group";
 import { IAddGroup } from "../use-cases/addGroup";
+
+interface IAddGroupResponse extends httpResponseType {
+    body: {
+        success: boolean;
+        data: IGroup | undefined;
+        error: string;
+    };
+}
 
 export default function makeAddGroupController({ addGroup }: IAddGroup) {
     return async function addGroupController(
         httpRequest: IHttpRequest
-    ): Promise<httpResponseType> {
+    ): Promise<IAddGroupResponse> {
         const headers: { [key: string]: string } = {
             "Content-Type": "application/json",
         };
@@ -28,7 +37,7 @@ export default function makeAddGroupController({ addGroup }: IAddGroup) {
                 statusCode: 400,
                 body: {
                     success: false,
-                    data: [],
+                    data: undefined,
                     error: error.message,
                 },
             };
