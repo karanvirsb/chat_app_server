0d97c0203010ed32c489cfe2b37cb7f4e8856a56
mrboparai <karanvirboparai123@gmail.com>
2022-10-07 09:54:14 -0400

    created an add user to group controller

---
+++ src/Features/group/controllers/add-userToGroup.ts
@@ -0,0 +1,48 @@
+import {
+    IHttpRequest,
+    httpResponseType,
+} from "../../../express-callback/index";
+import { IAddUserToGroup } from "../use-cases/addUserToGroup";
+import { groupUsers } from "../data-access/group-db";
+
+interface IAddUserToGroupResponse extends httpResponseType {
+    body: {
+        success: boolean;
+        data: groupUsers | undefined;
+        error: string;
+    };
+}
+
+export default function makeAddUserToGroupController({
+    addUserToGroup,
+}: IAddUserToGroup) {
+    return async function addUserToGroupController(
+        httpRequest: IHttpRequest
+    ): Promise<IAddUserToGroupResponse> {
+        const headers: { [key: string]: string } = {
+            "Content-Type": "application/json",
+        };
+        try {
+            const addedUser = await addUserToGroup(
+                httpRequest.body.groupId,
+                httpRequest.body.userId
+            );
+            return {
+                headers,
+                statusCode: 200,
+                body: addedUser,
+            };
+        } catch (error: any) {
+            console.log(error);
+            return {
+                headers,
+                statusCode: 400,
+                body: {
+                    success: false,
+                    data: undefined,
+                    error: error.message,
+                },
+            };
+        }
+    };
+}
