a3e1b061abd79d7977853a76520501936b6d1d01
mrboparai <karanvirboparai123@gmail.com>
2022-10-07 08:50:53 -0400

    added get group by id controller

--- src/Features/group/controllers/index.ts
+++ src/Features/group/controllers/index.ts
@@ -1,6 +1,7 @@
 import groupService from "../use-cases";
 import makeAddGroupController from "./add-group";
 import makeDeleteGroupController from "./delete-group";
+import makeGetGroupByIdController from "./get-byGroupId";
 
 const addGroupController = makeAddGroupController({
     addGroup: groupService.addGroup,
@@ -8,12 +9,16 @@ const addGroupController = makeAddGroupController({
 const deleteGroupController = makeDeleteGroupController({
     deleteGroup: groupService.deleteGroup,
 });
+const getGroupByIdController = makeGetGroupByIdController({
+    getGroupById: groupService.getGroupById,
+});
 
 const groupControllers = Object.freeze({
     addGroupController,
     deleteGroupController,
+    getGroupByIdController,
 });
 
 export default groupControllers;
 
-export { addGroupController, deleteGroupController };
+export { addGroupController, deleteGroupController, getGroupByIdController };
