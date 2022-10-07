6d4b1fc4996dc5e3e546c9127859c8387aa8d0c2
mrboparai <karanvirboparai123@gmail.com>
2022-10-07 09:33:58 -0400

    created tests for update group name controller

---
+++ src/Features/group/controllers/update-groupName.spec.ts
@@ -0,0 +1,154 @@
+import makeGroupDb from "../data-access/group-db";
+import makeDb, { clearDb } from "../../../../__test__/fixures/db";
+import makeAddGroup from "../use-cases/addGroup";
+import { moderateName } from "../../../Utilities/moderateText";
+import makeFakeGroup from "../../../../__test__/fixures/group";
+import makeUpdateGroupNameController from "./update-groupName";
+import makeUpdateGroupName from "../use-cases/updateGroupName";
+import sanitizeHtml from "sanitize-html";
+
+const handleModeration = async (name: string) => {
+    return await moderateName(name);
+};
+
+function sanitizeText(text: string) {
+    return sanitizeHtml(text);
+}
+
+describe("Update group name controller", () => {
+    // const groupRequest = {
+    //     body: {},
+    //     headers: {},
+    //     ip: "",
+    //     method: "",
+    //     params: {},
+    //     path: "",
+    //     query: {},
+    // };
+
+    const groupDb = makeGroupDb({ makeDb });
+    const addGroup = makeAddGroup({ groupDb, handleModeration });
+    const updateGroupName = makeUpdateGroupName({
+        groupDb,
+        handleModeration,
+        sanitizeName: sanitizeText,
+    });
+    const updateGroupNameController = makeUpdateGroupNameController({
+        updateGroupName,
+    });
+
+    beforeEach(async () => {
+        await clearDb("groupt");
+        await clearDb('"groupUsers"');
+    });
+
+    afterAll(async () => {
+        await clearDb("groupt");
+        await clearDb('"groupUsers"');
+    });
+
+    test("SUCCESS: update group name", async () => {
+        const group = await makeFakeGroup();
+        const groupRequest = {
+            body: {
+                groupId: group.groupId,
+                newGroupName: "Coders",
+            },
+            headers: { "Content-Type": "application/json" },
+            ip: "",
+            method: "POST",
+            params: {},
+            path: "",
+            query: {},
+        };
+
+        const updatedGroup = await updateGroupNameController(groupRequest);
+        expect(updatedGroup.body.data?.groupName).toBe("Coders");
+    });
+
+    test("ERROR: group id was not given for updateGroupNameController", async () => {
+        const group = await makeFakeGroup();
+        const groupRequest = {
+            body: {
+                groupId: "",
+                newGroupName: "Coders",
+            },
+            headers: { "Content-Type": "application/json" },
+            ip: "",
+            method: "POST",
+            params: {},
+            path: "",
+            query: {},
+        };
+
+        const updatedGroup = await updateGroupNameController(groupRequest);
+
+        expect(updatedGroup.statusCode).toBe(400);
+        expect(updatedGroup.body.error).toBe("Group Id needs to be supplied");
+    });
+
+    test("ERROR: group name was not given for updateGroupNameController", async () => {
+        const group = await makeFakeGroup();
+        const groupRequest = {
+            body: {
+                groupId: group.groupId,
+                newGroupName: "",
+            },
+            headers: { "Content-Type": "application/json" },
+            ip: "",
+            method: "POST",
+            params: {},
+            path: "",
+            query: {},
+        };
+
+        const updatedGroup = await updateGroupNameController(groupRequest);
+
+        expect(updatedGroup.statusCode).toBe(400);
+        expect(updatedGroup.body.error).toBe(
+            "A new group name must be supplied"
+        );
+    });
+
+    test("ERROR: contains profanity:  updateGroupNameController", async () => {
+        const group = await makeFakeGroup();
+        const groupRequest = {
+            body: {
+                groupId: group.groupId,
+                newGroupName: "bullshit",
+            },
+            headers: { "Content-Type": "application/json" },
+            ip: "",
+            method: "POST",
+            params: {},
+            path: "",
+            query: {},
+        };
+
+        const updatedGroup = await updateGroupNameController(groupRequest);
+
+        expect(updatedGroup.body.error).toBe("Group name contains profanity");
+    });
+
+    test("ERROR: contains html:  updateGroupNameController", async () => {
+        const group = await makeFakeGroup();
+        const groupRequest = {
+            body: {
+                groupId: group.groupId,
+                newGroupName: "<html></html>",
+            },
+            headers: { "Content-Type": "application/json" },
+            ip: "",
+            method: "POST",
+            params: {},
+            path: "",
+            query: {},
+        };
+
+        const updatedGroup = await updateGroupNameController(groupRequest);
+
+        expect(updatedGroup.body.error).toBe(
+            "Group name must contain valid characters"
+        );
+    });
+});
