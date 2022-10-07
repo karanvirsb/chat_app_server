fd43dabffac9b5906a945d159722515399338c72
mrboparai <karanvirboparai123@gmail.com>
2022-10-07 08:56:48 -0400

    added get group by invite code controller

--- src/Features/group/controllers/index.ts
+++ src/Features/group/controllers/index.ts
@@ -2,6 +2,7 @@ import groupService from "../use-cases";
 import makeAddGroupController from "./add-group";
 import makeDeleteGroupController from "./delete-group";
 import makeGetGroupByIdController from "./get-byGroupId";
+import makeGetGroupByInviteCodeController from "./get-byGroupInviteCode";
 
 const addGroupController = makeAddGroupController({
     addGroup: groupService.addGroup,
@@ -13,12 +14,22 @@ const getGroupByIdController = makeGetGroupByIdController({
     getGroupById: groupService.getGroupById,
 });
 
+const getGroupByInviteCodeController = makeGetGroupByInviteCodeController({
+    getGroupByInviteCode: groupService.getGroupByInviteCode,
+});
+
 const groupControllers = Object.freeze({
     addGroupController,
     deleteGroupController,
     getGroupByIdController,
+    getGroupByInviteCodeController,
 });
 
 export default groupControllers;
 
-export { addGroupController, deleteGroupController, getGroupByIdController };
+export {
+    addGroupController,
+    deleteGroupController,
+    getGroupByIdController,
+    getGroupByInviteCodeController,
+};
