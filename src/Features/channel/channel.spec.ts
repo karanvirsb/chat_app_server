83c286453b08b36040b81b4d7e2063e08a86350c
mrboparai <karanvirboparai123@gmail.com>
2022-10-08 11:16:08 -0400

    added tests for channel

---
+++ src/Features/channel/channel.spec.ts
@@ -0,0 +1,48 @@
+import { IChannel } from "./channel";
+import makeChannel from ".";
+
+describe("Channel test", () => {
+    const channelData: IChannel = {
+        channelId: "123456789",
+        channelName: "coolName",
+        dateCreated: new Date().toLocaleDateString(),
+        groupId: "123456789",
+    };
+    test("Channel make successfully", () => {
+        const channel = makeChannel(channelData);
+        expect(channel.getChannelName()).toBe("coolName");
+    });
+
+    test("ERROR: name should not contain html", () => {
+        expect(() =>
+            makeChannel({
+                ...channelData,
+                channelName: "<html></html>",
+            })
+        ).toThrow("Channel name should contain valid characters");
+    });
+
+    test("ERROR: channel name should be between 3-50", () => {
+        expect(() =>
+            makeChannel({ ...channelData, channelName: "12" })
+        ).toThrow("Channel name should be between 3 to 50 characters long");
+    });
+
+    test("ERROR: group id does not exist", () => {
+        expect(() => makeChannel({ ...channelData, groupId: "" })).toThrow(
+            "Group Id needs to be supplied"
+        );
+    });
+
+    test("ERROR: date needed", () => {
+        expect(() => makeChannel({ ...channelData, dateCreated: "" })).toThrow(
+            "Date needs to be supplied"
+        );
+    });
+
+    test("ERROR: channel id is needed", () => {
+        expect(() => makeChannel({ ...channelData, channelId: "" })).toThrow(
+            "Channel Id needs to be supplied"
+        );
+    });
+});
