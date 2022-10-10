0b7ac08fc4242707e930ae5746f4e403a5d6a7c5
mrboparai <karanvirboparai123@gmail.com>
2022-10-10 12:12:12 -0400

    added channel set up

--- server.ts
+++ server.ts
@@ -1,9 +1,11 @@
 const PORT = 8000;
 import app from "./app";
+import setupChannelDb from "./src/Features/channel/db";
 
 import setUpGroupDb from "./src/Features/group/db";
 
 app.listen(PORT, () => {
     console.log(`listening on port: ${PORT}`);
     setUpGroupDb();
+    setupChannelDb();
 });
