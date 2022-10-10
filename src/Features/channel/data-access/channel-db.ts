8cbbe711d743770c445022c9a35746ba2a77a8f8
mrboparai <karanvirboparai123@gmail.com>
2022-10-10 10:01:47 -0400

    created channel db

--- src/Features/channel/data-access/channel-db.ts
+++ src/Features/channel/data-access/channel-db.ts
@@ -1,7 +1,20 @@
 import { IChannelDb } from ".";
+import { IChannel } from "../channel";
 
 type props = {
     makeDb: IChannelDb["makeDb"];
 };
 
+type returningChannelData = Promise<{
+    success: boolean;
+    data: IChannel | undefined;
+    error: string;
+}>;
+
 export default function makeChannelDb({ makeDb }: props) {}
+
+// create channel (channelInfo, groupId);
+// delete channel (channelId);
+// update channelName (channelId, newName);
+// get channel by Id (channelId);
+// get Channels by group Id (groupId);
