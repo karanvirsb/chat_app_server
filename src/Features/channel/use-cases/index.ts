1f6ee61e3e591ac523cf80a8574214a526d34d82
mrboparai <karanvirboparai123@gmail.com>
2022-10-11 10:35:49 -0400

    added get channel by id use case

--- src/Features/channel/use-cases/index.ts
+++ src/Features/channel/use-cases/index.ts
@@ -4,6 +4,7 @@ import { handleModerationType } from "./createChannel";
 import channelDb from "../data-access";
 import makeDeleteChannel from "./deleteChannel";
 import makeUpdateChannelName from "./updateChannelName";
+import makeGetChannelById from "./getChannelById";
 const handleModeration: handleModerationType = async (channelName: string) => {
     return await moderateName(channelName);
 };
@@ -14,13 +15,15 @@ const updateChannelName = makeUpdateChannelName({
     handleModeration,
     channelDb,
 });
+const getChannelById = makeGetChannelById({ channelDb });
 
 const channelService = Object.freeze({
     createChannel,
     deleteChannel,
     updateChannelName,
+    getChannelById,
 });
 
 export default channelService;
 
-export { createChannel, deleteChannel, updateChannelName };
+export { createChannel, deleteChannel, updateChannelName, getChannelById };
