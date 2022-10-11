5cba2d09c4f57fd1b4999077c090368a352303eb
mrboparai <karanvirboparai123@gmail.com>
2022-10-11 10:27:19 -0400

    create delete channel tests

---
+++ src/Features/channel/use-cases/deleteChannel.spec.ts
@@ -0,0 +1,49 @@
+import makeChannelDb from "../data-access/channel-db";
+import makeDb, { clearDb } from "../../../../__test__/fixures/db";
+import makeCreateChannel, { handleModerationType } from "./createChannel";
+import makeFakeChannel from "../../../../__test__/fixures/channel";
+import makeDeleteChannel from "./deleteChannel";
+import { moderateName } from "../../../Utilities/moderateText";
+
+describe.skip("deleting channel use case", () => {
+    const handleModeration: handleModerationType = async (
+        channelName: string
+    ) => {
+        return await moderateName(channelName);
+    };
+
+    const channelDb = makeChannelDb({ makeDb });
+    const deleteChannel = makeDeleteChannel({
+        channelDb,
+    });
+    const createChannel = makeCreateChannel({
+        handleModeration,
+        channelDb,
+    });
+
+    afterEach(async () => {
+        clearDb("channelt");
+    });
+
+    test("SUCCESS: deleting channel", async () => {
+        const channel = await makeFakeChannel();
+        channel.groupId = "123";
+        const addedChannel = await createChannel(channel);
+
+        const deletedChannel = await deleteChannel(channel.channelId);
+        expect(deletedChannel.data?.channelId).toBe(channel.channelId);
+    });
+
+    test("ERROR: channel id not provided", async () => {
+        const channel = await makeFakeChannel();
+        channel.groupId = "123";
+        const addedChannel = await createChannel(channel);
+
+        try {
+            const deletedChannel = await deleteChannel("");
+        } catch (error) {
+            if (error instanceof Error)
+                expect(error.message).toBe("Channel Id needs to be supplied");
+        }
+    });
+});
