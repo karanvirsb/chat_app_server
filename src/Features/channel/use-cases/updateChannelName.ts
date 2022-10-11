9eca1d852482b627cb4d5776bdfa0237bf849379
mrboparai <karanvirboparai123@gmail.com>
2022-10-11 10:28:22 -0400

    created use case for updating channel name

---
+++ src/Features/channel/use-cases/updateChannelName.ts
@@ -0,0 +1,51 @@
+import { IChannel } from "../channel";
+import { IMakeChannelDb } from "../data-access/channel-db";
+import makeChannel from "../index";
+
+export type handleModerationType = {
+    (name: string): Promise<number | boolean>;
+};
+
+type props = {
+    channelDb: IMakeChannelDb["returnType"];
+    handleModeration: handleModerationType;
+};
+
+type returnData = Promise<{
+    success: boolean;
+    data: IChannel | undefined;
+    error: string;
+}>;
+
+export default function makeUpdateChannelName({
+    handleModeration,
+    channelDb,
+}: props) {
+    return async function updateChannelName(
+        channelId: string,
+        newName: string
+    ): Promise<returnData> {
+        if (!channelId) throw new Error("Channel Id needs to be supplied");
+        if (!newName) throw new Error("New Channel Name needs to be supplied");
+
+        const moderatedName = await handleModeration(newName);
+
+        if (moderatedName) {
+            return {
+                success: false,
+                data: undefined,
+                error: "New Channel Name contains profanity",
+            };
+        }
+
+        if (moderatedName === -1) {
+            return {
+                success: false,
+                data: undefined,
+                error: "Server Error, please try again.",
+            };
+        }
+
+        return await channelDb.updateChannelName(channelId, newName);
+    };
+}
