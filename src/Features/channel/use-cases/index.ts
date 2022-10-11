e9db386261276ef11fe44a0b54d21c704b029f95
mrboparai <karanvirboparai123@gmail.com>
2022-10-11 10:28:33 -0400

    added use case updating channel name

--- src/Features/channel/use-cases/index.ts
+++ src/Features/channel/use-cases/index.ts
@@ -3,18 +3,24 @@ import makeCreateChannel from "./createChannel";
 import { handleModerationType } from "./createChannel";
 import channelDb from "../data-access";
 import makeDeleteChannel from "./deleteChannel";
+import makeUpdateChannelName from "./updateChannelName";
 const handleModeration: handleModerationType = async (channelName: string) => {
     return await moderateName(channelName);
 };
 
 const createChannel = makeCreateChannel({ handleModeration, channelDb });
 const deleteChannel = makeDeleteChannel({ channelDb });
+const updateChannelName = makeUpdateChannelName({
+    handleModeration,
+    channelDb,
+});
 
 const channelService = Object.freeze({
     createChannel,
     deleteChannel,
+    updateChannelName,
 });
 
 export default channelService;
 
-export { createChannel, deleteChannel };
+export { createChannel, deleteChannel, updateChannelName };
