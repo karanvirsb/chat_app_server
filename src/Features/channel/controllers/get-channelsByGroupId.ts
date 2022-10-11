111c9e32380320ea3cedb562e89f5a0576587e4a
mrboparai <karanvirboparai123@gmail.com>
2022-10-11 11:55:27 -0400

    created a get channels by group id controller

---
+++ src/Features/channel/controllers/get-channelsByGroupId.ts
@@ -0,0 +1,47 @@
+import {
+    IHttpRequest,
+    httpResponseType,
+} from "../../../express-callback/index";
+import { IChannel } from "../channel";
+import { IGetChannelsByGroupIdUseCase } from "../use-cases/getChannelsByGroupId";
+
+interface IGetChannelsByGroupIdResponse extends httpResponseType {
+    body: {
+        success: boolean;
+        data: IChannel[] | undefined;
+        error: string;
+    };
+}
+
+export default function makeGetChannelsByGroupIdController({
+    getChannelsByGroupId,
+}: IGetChannelsByGroupIdUseCase) {
+    return async function getChannelsByGroupIdController(
+        httpRequest: IHttpRequest
+    ): Promise<IGetChannelsByGroupIdResponse> {
+        const headers: { [key: string]: string } = {
+            "Content-Type": "application/json",
+        };
+        try {
+            const foundChannels = await getChannelsByGroupId(
+                httpRequest.params.groupId
+            );
+            return {
+                headers,
+                statusCode: 200,
+                body: foundChannels,
+            };
+        } catch (error: any) {
+            console.log(error);
+            return {
+                headers,
+                statusCode: 400,
+                body: {
+                    success: false,
+                    data: undefined,
+                    error: error.message,
+                },
+            };
+        }
+    };
+}
