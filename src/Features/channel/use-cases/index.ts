7791f2f8d9a66257df9ce58b54b78d96e1993cef
mrboparai <karanvirboparai123@gmail.com>
2022-10-11 10:18:43 -0400

    added delete channel use case

--- src/Features/channel/use-cases/index.ts
+++ src/Features/channel/use-cases/index.ts
@@ -2,16 +2,19 @@ import { moderateName } from "../../../Utilities/moderateText";
 import makeCreateChannel from "./createChannel";
 import { handleModerationType } from "./createChannel";
 import channelDb from "../data-access";
+import makeDeleteChannel from "./deleteChannel";
 const handleModeration: handleModerationType = async (channelName: string) => {
     return await moderateName(channelName);
 };
 
 const createChannel = makeCreateChannel({ handleModeration, channelDb });
+const deleteChannel = makeDeleteChannel({ channelDb });
 
 const channelService = Object.freeze({
     createChannel,
+    deleteChannel,
 });
 
 export default channelService;
 
-export { createChannel };
+export { createChannel, deleteChannel };
