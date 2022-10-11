f199223c14a687c09831c8d5d9b5b39ca31aa3ec
mrboparai <karanvirboparai123@gmail.com>
2022-10-11 12:20:45 -0400

    created a test for getting multiple channels

---
+++ src/Features/channel/controllers/get-channelsByGroupId.spec.ts
@@ -0,0 +1,79 @@
+import makeChannelDb from "../data-access/channel-db";
+import makeDb, { clearDb } from "../../../../__test__/fixures/db";
+import makeCreateChannel from "../use-cases/createChannel";
+import { moderateName } from "../../../Utilities/moderateText";
+import makeFakeChannel from "../../../../__test__/fixures/channel";
+import makeGetChannelsByGroupIdController from "./get-channelsByGroupId";
+import makeGetChannelsByGroupId from "../use-cases/getChannelsByGroupId";
+
+describe.skip("Get channels by group id controller", () => {
+    // const channelRequest = {
+    //     body: {},
+    //     headers: {},
+    //     ip: "",
+    //     method: "",
+    //     params: {},
+    //     path: "",
+    //     query: {},
+    // };
+    const handleModeration = async (name: string) => {
+        return await moderateName(name);
+    };
+
+    const channelDb = makeChannelDb({ makeDb });
+    const createChannel = makeCreateChannel({ handleModeration, channelDb });
+
+    const getChannelsByGroupId = makeGetChannelsByGroupId({ channelDb });
+    const getChannelsByGroupIdController = makeGetChannelsByGroupIdController({
+        getChannelsByGroupId,
+    });
+
+    afterEach(async () => {
+        clearDb("channelt");
+    });
+
+    test("SUCCESS: get channel by group id", async () => {
+        const channel = await makeFakeChannel();
+        channel.groupId = "123";
+        const channelRequest = {
+            body: { groupId: channel.groupId },
+            headers: {},
+            ip: "",
+            method: "POST",
+            params: {},
+            path: "",
+            query: {},
+        };
+
+        const createdChannel = await createChannel(channel);
+
+        const foundChannels = await getChannelsByGroupIdController(
+            channelRequest
+        );
+        if (foundChannels.body.data)
+            expect(foundChannels.body.data[0].channelId).toBe(
+                channel.channelId
+            );
+    });
+
+    test("ERROR: group id does not exist", async () => {
+        const channel = await makeFakeChannel();
+        channel.groupId = "";
+        const channelRequest = {
+            body: { channelId: channel.channelId },
+            headers: {},
+            ip: "",
+            method: "POST",
+            params: {},
+            path: "",
+            query: {},
+        };
+
+        const createdChannel = await createChannel(channel);
+
+        const foundChannels = await getChannelsByGroupIdController(
+            channelRequest
+        );
+        expect(foundChannels.body.error).toBe("Group Id needs to be supplied");
+    });
+});
