64458e7e9951d07e0cff6dee18960a6642f5caea
mrboparai <karanvirboparai123@gmail.com>
2022-10-11 11:54:50 -0400

    created a delete channel controller

---
+++ src/Features/channel/controllers/delete-channel.ts
@@ -0,0 +1,47 @@
+import {
+    IHttpRequest,
+    httpResponseType,
+} from "../../../express-callback/index";
+import { IChannel } from "../channel";
+import { IDeleteChannelUseCase } from "../use-cases/deleteChannel";
+
+interface IDeleteChannelResponse extends httpResponseType {
+    body: {
+        success: boolean;
+        data: IChannel | undefined;
+        error: string;
+    };
+}
+
+export default function makeDeleteChannelController({
+    deleteChannel,
+}: IDeleteChannelUseCase) {
+    return async function deleteChannelController(
+        httpRequest: IHttpRequest
+    ): Promise<IDeleteChannelResponse> {
+        const headers: { [key: string]: string } = {
+            "Content-Type": "application/json",
+        };
+        try {
+            const deletedChannel = await deleteChannel(
+                httpRequest.body.channelId
+            );
+            return {
+                headers,
+                statusCode: 200,
+                body: deletedChannel,
+            };
+        } catch (error: any) {
+            console.log(error);
+            return {
+                headers,
+                statusCode: 400,
+                body: {
+                    success: false,
+                    data: undefined,
+                    error: error.message,
+                },
+            };
+        }
+    };
+}
