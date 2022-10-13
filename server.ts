593be6240a7240a7d32e994d1ac975b84fe22d89
mrboparai <karanvirboparai123@gmail.com>
2022-10-13 10:39:50 -0400

    added set up message db

--- server.ts
+++ server.ts
@@ -3,9 +3,11 @@ import app from "./app";
 import setupChannelDb from "./src/Features/channel/db";
 
 import setUpGroupDb from "./src/Features/group/db";
+import setupMessageDb from "./src/Features/message/db";
 
 app.listen(PORT, () => {
     console.log(`listening on port: ${PORT}`);
     setUpGroupDb();
     setupChannelDb();
+    setupMessageDb();
 });
