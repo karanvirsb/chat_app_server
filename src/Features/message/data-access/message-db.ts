27a4ca82329eb33bf7e1a711c970aa9bd58f1860
mrboparai <karanvirboparai123@gmail.com>
2022-10-13 11:17:17 -0400

    created a create message db

--- src/Features/message/data-access/message-db.ts
+++ src/Features/message/data-access/message-db.ts
@@ -1,15 +1,79 @@
 import { IMessageDb } from ".";
+import { IMessage } from "../message";
 
 type props = {
     makeDb: IMessageDb["makeDb"];
 };
 
+type returningMessageData = Promise<{
+    success: boolean;
+    data: IMessage | undefined;
+    error: string;
+}>;
+
+type returningMessagesData = Promise<{
+    success: boolean;
+    data: IMessage[] | undefined;
+    error: string;
+}>;
+
 export interface IMakeMessageDb {
-    returnType: Readonly<{}>;
+    returnType: Readonly<{
+        createMessage: (messageInfo: IMessage) => Promise<returningMessageData>;
+    }>;
 }
 
 export default function makeMessageDb({
     makeDb,
 }: props): IMakeMessageDb["returnType"] {
-    return Object.freeze({});
+    return Object.freeze({
+        createMessage,
+    });
+
+    // create message
+    async function createMessage(
+        messageInfo: IMessage
+    ): Promise<returningMessageData> {
+        const db = await makeDb();
+        try {
+            const query = `INSERT INTO messaget VALUES (
+                '${messageInfo.messageId}',
+                to_timestamp(${messageInfo.dateCreated.getTime()}/1000),
+                ,
+                ,
+                '${messageInfo.text}', 
+                '${messageInfo.userId}', 
+                '${messageInfo.channelId}'
+                ) RETURNING *;`;
+            const res = await db.query(query);
+
+            if (res.rowCount === 1) {
+                const message: IMessage = res.rows[0];
+                message.dateCreated = new Date(message.dateCreated);
+                return { success: true, data: message, error: "" };
+            } else {
+                return {
+                    success: true,
+                    data: undefined,
+                    error: "Could not create the channel.",
+                };
+            }
+        } catch (error) {
+            console.log(
+                "🚀 ~ file: message-db.ts ~ line 54 ~ error ~ createMessage",
+                error
+            );
+            return {
+                success: false,
+                data: undefined,
+                error: error + "",
+            };
+        } finally {
+            db.release();
+        }
+    }
+    // delete message
+    // get message by id
+    // get messages by channel id
+    // update message
 }
