7b962c75df13ef47111ec0f9677e73bb7ff839cd
mrboparai <karanvirboparai123@gmail.com>
2022-10-15 17:12:11 -0400

    added new use case of getting messages

--- src/Features/message/use-cases/index.ts
+++ src/Features/message/use-cases/index.ts
@@ -2,17 +2,20 @@ import makeCreateMessage from "./createMessage";
 import messageDb from "../data-access";
 import makeDeleteMessage from "./deleteMessage";
 import makeGetMessageById from "./getMessageById";
+import makegetMessagesByChannelId from "./getMessagesByChannelId";
 
 const createMessage = makeCreateMessage({ messageDb });
 const deleteMessage = makeDeleteMessage({ messageDb });
 const getMessageById = makeGetMessageById({ messageDb });
+const getMessagesByChannelId = makegetMessagesByChannelId({ messageDb });
 
 const messageService = Object.freeze({
     createMessage,
     deleteMessage,
     getMessageById,
+    getMessagesByChannelId,
 });
 
 export default messageService;
 
-export { createMessage, deleteMessage, getMessageById };
+export { createMessage, deleteMessage, getMessageById, getMessagesByChannelId };
