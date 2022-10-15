9d52accb1fd1acc60d35e3c08748ced71fa24c65
mrboparai <karanvirboparai123@gmail.com>
2022-10-15 17:08:38 -0400

    created use case for getting a message by id

---
+++ src/Features/message/use-cases/getMessageById.ts
@@ -0,0 +1,20 @@
+import { IMakeMessageDb } from "../data-access/message-db";
+import { IMessage } from "../message";
+
+type props = {
+    messageDb: IMakeMessageDb["returnType"];
+};
+
+type returnData = Promise<{
+    success: boolean;
+    data: IMessage | undefined;
+    error: string;
+}>;
+
+export default function makeGetMessageById({ messageDb }: props) {
+    return async function getMessageById(messageId: string): returnData {
+        if (!messageId) throw new Error("Message Id needs to be supplied.");
+
+        return messageDb.getMessageById(messageId);
+    };
+}
