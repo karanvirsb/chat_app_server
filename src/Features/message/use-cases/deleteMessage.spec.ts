f4df0c7d63a7a9a45fd3e653db39b8a7a5df39cb
mrboparai <karanvirboparai123@gmail.com>
2022-10-15 16:59:31 -0400

    created a test for deleting message

---
+++ src/Features/message/use-cases/deleteMessage.spec.ts
@@ -0,0 +1,36 @@
+import makeMessageDb from "../data-access/message-db";
+import makeDb, { clearDb } from "../../../../__test__/fixures/db";
+import makeDeleteMessage from "./deleteMessage";
+import makeCreateMessage from "./createMessage";
+import makeFakeMessage from "../../../../__test__/fixures/message";
+
+describe("Create message use case", () => {
+    const messageDb = makeMessageDb({ makeDb });
+    const createMessage = makeCreateMessage({ messageDb });
+    const deleteMessage = makeDeleteMessage({ messageDb });
+
+    afterEach(async () => {
+        clearDb("messaget");
+    });
+
+    test("SUCCESS: creating a message", async () => {
+        const message = await makeFakeMessage("123", "123");
+        const insertedMessage = await createMessage(message);
+
+        const deletedMessage = await deleteMessage(message.messageId);
+
+        expect(deletedMessage.data?.messageId).toBe(message.messageId);
+    });
+
+    test("ERROR: message id is missing", async () => {
+        const message = await makeFakeMessage("123", "123");
+        const insertedMessage = await createMessage(message);
+
+        try {
+            const deletedMessage = await deleteMessage(message.messageId);
+        } catch (error) {
+            if (error instanceof Error)
+                expect(error.message).toBe("Message Id needs to be supplied.");
+        }
+    });
+});
