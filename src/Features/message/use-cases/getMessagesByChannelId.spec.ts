2e325ed0089b73f8761cfa31253abd682b5e9e9e
mrboparai <karanvirboparai123@gmail.com>
2022-10-15 17:36:57 -0400

    added tests for getting messages by channel id

---
+++ src/Features/message/use-cases/getMessagesByChannelId.spec.ts
@@ -0,0 +1,44 @@
+import makeMessageDb from "../data-access/message-db";
+import makeDb, { clearDb } from "../../../../__test__/fixures/db";
+import makeCreateMessage from "./createMessage";
+import makeFakeMessage from "../../../../__test__/fixures/message";
+import makeGetMessagesByChannelId from "./getMessagesByChannelId";
+
+describe("Create message use case", () => {
+    const messageDb = makeMessageDb({ makeDb });
+    const createMessage = makeCreateMessage({ messageDb });
+    const getMessagesByChannelId = makeGetMessagesByChannelId({ messageDb });
+
+    afterEach(async () => {
+        clearDb("messaget");
+    });
+
+    test("SUCCESS: getting a message", async () => {
+        const message = await makeFakeMessage("123", "123");
+        const insertedMessage = await createMessage(message);
+
+        const foundMessage = await getMessagesByChannelId(
+            message.messageId,
+            message.dateCreated,
+            10
+        );
+
+        if (foundMessage.data)
+            expect(foundMessage.data[0].messageId).toBe(message.messageId);
+    });
+
+    test("ERROR: missing message id ", async () => {
+        const message = await makeFakeMessage("123", "123");
+        const insertedMessage = await createMessage(message);
+
+        try {
+            const foundMessage = await getMessagesByChannelId(
+                "",
+                message.dateCreated
+            );
+        } catch (error) {
+            if (error instanceof Error)
+                expect(error.message).toBe("Message Id needs to be supplied.");
+        }
+    });
+});
