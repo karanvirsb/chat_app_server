e7dc17eef947ffe12db0c9896e207657967a3423
mrboparai <karanvirboparai123@gmail.com>
2022-10-15 17:12:00 -0400

    created a use case to get messages by channel id

---
+++ src/Features/message/use-cases/getMessagesByChannelId.ts
@@ -0,0 +1,25 @@
+import { IMakeMessageDb } from "../data-access/message-db";
+import { IMessage } from "../message";
+
+type props = {
+    messageDb: IMakeMessageDb["returnType"];
+};
+
+type returnData = Promise<{
+    success: boolean;
+    data: IMessage[] | undefined;
+    error: string;
+}>;
+
+export default function makegetMessagesByChannelId({ messageDb }: props) {
+    return async function getMessagesByChannelId(
+        messageId: string,
+        dateCreated: Date,
+        limit: number = 15
+    ): returnData {
+        if (!messageId) throw new Error("Message Id needs to be supplied.");
+        if (!dateCreated) throw new Error("Date Created needs to be supplied.");
+
+        return messageDb.getMessagesByChannelId(messageId, dateCreated, limit);
+    };
+}
