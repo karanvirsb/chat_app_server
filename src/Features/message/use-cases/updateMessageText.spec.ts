ae2a33ab5cf68a082194dcb19d772d13e8acf68c
mrboparai <karanvirboparai123@gmail.com>
2022-10-15 18:20:16 -0400

    created test for updating message text

---
+++ src/Features/message/use-cases/updateMessageText.spec.ts
@@ -0,0 +1,54 @@
+import makeMessageDb from "../data-access/message-db";
+import makeDb, { clearDb } from "../../../../__test__/fixures/db";
+import makeCreateMessage from "./createMessage";
+import makeFakeMessage from "../../../../__test__/fixures/message";
+import makeUpdateMessageText from "./updateMessageText";
+
+describe("Create message use case", () => {
+    const messageDb = makeMessageDb({ makeDb });
+    const createMessage = makeCreateMessage({ messageDb });
+    const updateMessageText = makeUpdateMessageText({ messageDb });
+
+    afterEach(async () => {
+        clearDb("messaget");
+    });
+
+    test("SUCCESS: getting a message", async () => {
+        const message = await makeFakeMessage("123", "123");
+        const insertedMessage = await createMessage(message);
+
+        const updatedMessage = await updateMessageText(
+            message.messageId,
+            "Coders are awesome"
+        );
+
+        expect(updatedMessage.data?.text).toBe("Coders are awesome");
+    });
+
+    test("ERROR: missing message id ", async () => {
+        const message = await makeFakeMessage("123", "123");
+        const insertedMessage = await createMessage(message);
+
+        try {
+            const updateMessage = await updateMessageText("", "Coding");
+        } catch (error) {
+            if (error instanceof Error)
+                expect(error.message).toBe("Message Id needs to be supplied.");
+        }
+    });
+
+    test("ERROR: missing text ", async () => {
+        const message = await makeFakeMessage("123", "123");
+        const insertedMessage = await createMessage(message);
+
+        try {
+            const updateMessage = await updateMessageText(
+                message.messageId,
+                ""
+            );
+        } catch (error) {
+            if (error instanceof Error)
+                expect(error.message).toBe("Text needs to be supplied.");
+        }
+    });
+});
