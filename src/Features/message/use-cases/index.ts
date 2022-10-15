d18263e60f48519a241b12fb6884116b077002d1
mrboparai <karanvirboparai123@gmail.com>
2022-10-15 17:08:48 -0400

    added use case of getting message by id

--- src/Features/message/use-cases/index.ts
+++ src/Features/message/use-cases/index.ts
@@ -1,15 +1,18 @@
 import makeCreateMessage from "./createMessage";
 import messageDb from "../data-access";
 import makeDeleteMessage from "./deleteMessage";
+import makeGetMessageById from "./getMessageById";
 
 const createMessage = makeCreateMessage({ messageDb });
 const deleteMessage = makeDeleteMessage({ messageDb });
+const getMessageById = makeGetMessageById({ messageDb });
 
 const messageService = Object.freeze({
     createMessage,
     deleteMessage,
+    getMessageById,
 });
 
 export default messageService;
 
-export { createMessage, deleteMessage };
+export { createMessage, deleteMessage, getMessageById };
