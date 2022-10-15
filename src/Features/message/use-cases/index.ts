bf26b369193c8b61e60e16f21dbe216b047994bd
mrboparai <karanvirboparai123@gmail.com>
2022-10-15 16:56:25 -0400

    added delete message use case

--- src/Features/message/use-cases/index.ts
+++ src/Features/message/use-cases/index.ts
@@ -1,12 +1,15 @@
 import makeCreateMessage from "./createMessage";
 import messageDb from "../data-access";
+import makeDeleteMessage from "./deleteMessage";
 
 const createMessage = makeCreateMessage({ messageDb });
+const deleteMessage = makeDeleteMessage({ messageDb });
 
 const messageService = Object.freeze({
     createMessage,
+    deleteMessage,
 });
 
 export default messageService;
 
-export { createMessage };
+export { createMessage, deleteMessage };
