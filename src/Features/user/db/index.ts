6ec7fde5a60b3a3d521bc9d8d8eb9000c8304967
mrboparai <karanvirboparai123@gmail.com>
2022-10-16 12:01:16 -0400

    created an user db set up

--- src/Features/user/db/index.ts
+++ src/Features/user/db/index.ts
@@ -1,1 +1,18 @@
-// TODO create userT table just in case
+import { makeDb } from "../data-access";
+import dotenv from "dotenv";
+dotenv.config();
+
+export default async function setupUserDb() {
+    console.log("setting up user database...");
+    // database will be created if it doesnt exist already
+
+    const db = await makeDb();
+    const result = await db.query(`
+    CREATE TABLE IF NOT EXISTS usert(
+      "userId" VARCHAR(100) REFERENCES emailpassword_users(user_id) ON DELETE CASCADE ON UPDATE CASCADE,
+      username VARCHAR(50) PRIMARY KEY, 
+      status VARCHAR(50));
+  `);
+    console.log("🚀 ~ file: index.ts ~ line 24 ~ setupUserDb ~ result", result);
+    console.log("User Database set up complete...");
+}
