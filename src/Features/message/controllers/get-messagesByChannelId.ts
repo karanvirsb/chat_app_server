0197f19ea348c3164a5e767ee079e2b28113dbcc
mrboparai <karanvirboparai123@gmail.com>
2022-10-16 10:59:02 -0400

    create a get messages by channel id controller

---
+++ src/Features/message/controllers/get-messagesByChannelId.ts
@@ -0,0 +1,50 @@
+import {
+    IHttpRequest,
+    httpResponseType,
+} from "../../../express-callback/index";
+import { IMessage } from "../message";
+import { IGetMessageByIdUseCase } from "../use-cases/getMessageById";
+import { IGetMessagesByChannelIdUseCase } from "../use-cases/getMessagesByChannelId";
+
+interface IGetMessagesByChannelIdResponse extends httpResponseType {
+    body: {
+        success: boolean;
+        data: IMessage[] | undefined;
+        error: string;
+    };
+}
+
+export default function makeGetMessagesByChannelIdController({
+    getMessagesByChannelId,
+}: IGetMessagesByChannelIdUseCase) {
+    return async function getMessagesByChannelIdController(
+        httpRequest: IHttpRequest
+    ): Promise<IGetMessagesByChannelIdResponse> {
+        const headers: { [key: string]: string } = {
+            "Content-Type": "application/json",
+        };
+        try {
+            const foundMessages = await getMessagesByChannelId(
+                httpRequest.params.messageId,
+                new Date(httpRequest.params.dateCreated),
+                parseInt(httpRequest.params.limit)
+            );
+            return {
+                headers,
+                statusCode: 200,
+                body: foundMessages,
+            };
+        } catch (error: any) {
+            console.log(error);
+            return {
+                headers,
+                statusCode: 400,
+                body: {
+                    success: false,
+                    data: undefined,
+                    error: error.message,
+                },
+            };
+        }
+    };
+}
