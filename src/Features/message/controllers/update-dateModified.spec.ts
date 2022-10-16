9604397cfc1c90bdc53a94bdd76524b7ee77f4a4
mrboparai <karanvirboparai123@gmail.com>
2022-10-16 11:48:48 -0400

    created a test for modified date

---
+++ src/Features/message/controllers/update-dateModified.spec.ts
@@ -0,0 +1,105 @@
+import makeDb, { clearDb } from "../../../../__test__/fixures/db";
+import makeFakeMessage from "../../../../__test__/fixures/message";
+import makeMessageDb from "../data-access/message-db";
+import makeCreateMessage from "../use-cases/createMessage";
+import makeUpdateDateModified from "../use-cases/updateDateModified";
+import makeUpdateDateModifiedController from "./update-dateModified";
+
+describe("updating message date modified controller", () => {
+    const messageDb = makeMessageDb({ makeDb });
+    const createMessage = makeCreateMessage({ messageDb });
+
+    const updateDateModified = makeUpdateDateModified({ messageDb });
+    const updateDateModifiedController = makeUpdateDateModifiedController({
+        updateDateModified,
+    });
+
+    afterEach(async () => {
+        await clearDb("messaget");
+    });
+
+    test("SUCCESS: updated date modified", async () => {
+        const message = await makeFakeMessage(
+            "123",
+            "5c0fc896-1af1-4c26-b917-550ac5eefa9e"
+        );
+        const messageRequest = {
+            body: {
+                messageId: message.messageId,
+                updateValue: new Date().toString(),
+            },
+            headers: {},
+            ip: "",
+            method: "",
+            params: {},
+            path: "",
+            query: {},
+        };
+
+        const createdMessage = await createMessage(message);
+
+        const updatedMessage = await updateDateModifiedController(
+            messageRequest
+        );
+
+        expect(updatedMessage.body.data?.dateModified).not.toEqual(
+            message.dateModified
+        );
+    });
+
+    test("ERROR: message id missing ", async () => {
+        const message = await makeFakeMessage(
+            "123",
+            "5c0fc896-1af1-4c26-b917-550ac5eefa9e"
+        );
+        const messageRequest = {
+            body: {
+                messageId: "",
+                updateValue: new Date().toString(),
+            },
+            headers: {},
+            ip: "",
+            method: "",
+            params: {},
+            path: "",
+            query: {},
+        };
+
+        const createdMessage = await createMessage(message);
+
+        const updatedMessage = await updateDateModifiedController(
+            messageRequest
+        );
+
+        expect(updatedMessage.body.error).toBe(
+            "Message Id needs to be supplied."
+        );
+    });
+
+    test("ERROR: date modified missing ", async () => {
+        const message = await makeFakeMessage(
+            "123",
+            "5c0fc896-1af1-4c26-b917-550ac5eefa9e"
+        );
+        const messageRequest = {
+            body: {
+                messageId: message.messageId,
+                updateValue: "",
+            },
+            headers: {},
+            ip: "",
+            method: "",
+            params: {},
+            path: "",
+            query: {},
+        };
+
+        const createdMessage = await createMessage(message);
+        const updatedMessage = await updateDateModifiedController(
+            messageRequest
+        );
+        expect(updatedMessage.body.error).toBe(
+            "Update Value needs to be a Date."
+        );
+    });
+});
