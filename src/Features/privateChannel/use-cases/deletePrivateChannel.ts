da441b87c199b615f78c44944e8eb1307d073d19
mrboparai <karanvirboparai123@gmail.com>
2022-10-31 09:35:22 -0400

    updated to private channel db

--- src/Features/privateChannel/use-cases/deletePrivateChannel.ts
+++ src/Features/privateChannel/use-cases/deletePrivateChannel.ts
@@ -2,7 +2,7 @@ import { IMakePrivateChannelDb } from "../data-access/privateChannel-db";
 import { IPrivateChannel } from "../privateChannel";
 
 type props = {
-    channelDb: IMakePrivateChannelDb["returnType"];
+    privateChannelDb: IMakePrivateChannelDb["returnType"];
 };
 
 type returnData = Promise<{
@@ -16,13 +16,13 @@ export interface IDeletePrivateChannelUseCase {
 }
 
 export default function makeDeletePrivateChannel({
-    channelDb,
+    privateChannelDb,
 }: props): IDeletePrivateChannelUseCase["deletePrivateChannel"] {
     return async function deletePrivateChannel(
         channelId: string
     ): Promise<returnData> {
         if (!channelId) throw new Error("Channel Id needs to be supplied");
 
-        return await channelDb.deletePrivateChannel(channelId);
+        return await privateChannelDb.deletePrivateChannel(channelId);
     };
 }
