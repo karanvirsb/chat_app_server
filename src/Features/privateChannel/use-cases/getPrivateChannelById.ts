41cd83276e722d49e71f114f745f9dd3d11df2ef
mrboparai <karanvirboparai123@gmail.com>
2022-10-31 09:36:04 -0400

    updated channelDb to privateChannelDb

--- src/Features/privateChannel/use-cases/getPrivateChannelById.ts
+++ src/Features/privateChannel/use-cases/getPrivateChannelById.ts
@@ -2,7 +2,7 @@ import { IMakePrivateChannelDb } from "../data-access/privateChannel-db";
 import { IPrivateChannel } from "../privateChannel";
 
 type props = {
-    channelDb: IMakePrivateChannelDb["returnType"];
+    privateChannelDb: IMakePrivateChannelDb["returnType"];
 };
 
 type returnData = Promise<{
@@ -16,13 +16,13 @@ export interface IGetPrivateChannelByIdUseCase {
 }
 
 export default function makeGetPrivateChannelById({
-    channelDb,
+    privateChannelDb,
 }: props): IGetPrivateChannelByIdUseCase["getPrivateChannelById"] {
     return async function getPrivateChannelById(
         channelId: string
     ): Promise<returnData> {
         if (!channelId) throw new Error("Channel Id needs to be supplied");
 
-        return await channelDb.getPrivateChannelById(channelId);
+        return await privateChannelDb.getPrivateChannelById(channelId);
     };
 }
