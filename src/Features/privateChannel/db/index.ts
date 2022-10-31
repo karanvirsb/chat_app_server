a5b29751b7ea81bd00e4ca5dc17d5195ea5604c0
mrboparai <karanvirboparai123@gmail.com>
2022-10-31 08:40:40 -0400

    created a set up for private channels

---
+++ src/Features/privateChannel/db/index.ts
@@ -0,0 +1,26 @@
+import { makeDb } from "../data-access";
+import dotenv from "dotenv";
+dotenv.config();
+
+export default async function setupPrivateChannelDb() {
+    console.log("setting up private channel database...");
+    // database will be created if it doesnt exist already
+
+    const db = await makeDb();
+    const result = await db.query(
+        `CREATE TABLE IF NOT EXISTS private_channels(
+            "channelId" VARCHAR(100) PRIMARY KEY,
+            "channelName" VARCHAR(50),
+            "dateCreated" timestamp,
+            "userId" VARCHAR(100) REFERENCES usert("userId") ON DELETE CASCADE ON UPDATE CASCADE,
+            "friendsId" VARCHAR(100) REFERENCES usert("userId") ON DELETE SET NULL ON UPDATE CASCADE
+            );
+        `
+    );
+    console.log(
+        "🚀 ~ file: index.ts ~ line 20 ~ setupChannelDb ~ result",
+        result
+    );
+
+    console.log("Private channel Database set up complete...");
+}
