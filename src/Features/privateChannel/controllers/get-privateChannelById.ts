b7b7ffbc5a02139d72cac1eb171b14f7bf657573
mrboparai <karanvirboparai123@gmail.com>
2022-11-01 08:59:49 -0400

    create get private channel by id

---
+++ src/Features/privateChannel/controllers/get-privateChannelById.ts
@@ -0,0 +1,47 @@
+import {
+    IHttpRequest,
+    httpResponseType,
+} from "../../../express-callback/index";
+import { IPrivateChannel } from "../privateChannel";
+import { IGetPrivateChannelByIdUseCase } from "../use-cases/getPrivateChannelById";
+
+interface IGetPrivateChannelByIdResponse extends httpResponseType {
+    body: {
+        success: boolean;
+        data: IPrivateChannel | undefined;
+        error: string;
+    };
+}
+
+export default function makeGetChannelByIdController({
+    getPrivateChannelById,
+}: IGetPrivateChannelByIdUseCase) {
+    return async function getChannelByIdController(
+        httpRequest: IHttpRequest
+    ): Promise<IGetPrivateChannelByIdResponse> {
+        const headers: { [key: string]: string } = {
+            "Content-Type": "application/json",
+        };
+        try {
+            const foundChannel = await getPrivateChannelById(
+                httpRequest.params.channelId
+            );
+            return {
+                headers,
+                statusCode: 200,
+                body: foundChannel,
+            };
+        } catch (error: any) {
+            console.log(error);
+            return {
+                headers,
+                statusCode: 400,
+                body: {
+                    success: false,
+                    data: undefined,
+                    error: error.message,
+                },
+            };
+        }
+    };
+}
