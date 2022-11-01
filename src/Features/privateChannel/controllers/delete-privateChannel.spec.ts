d5513c127b915ad33e6422b0c12809490a534511
mrboparai <karanvirboparai123@gmail.com>
2022-11-01 17:45:27 -0400

    created test

---
+++ src/Features/privateChannel/controllers/delete-privateChannel.spec.ts
@@ -0,0 +1,88 @@
+import makePrivateChannelDb from "../data-access/privateChannel-db";
+import makeDb, { clearDb } from "../../../../__test__/fixures/db";
+import makeCreatePrivateChannel from "../use-cases/createPrivateChannel";
+import { moderateName } from "../../../Utilities/moderateText";
+import makeCreatePrivateChannelController from "./create-privateChannel";
+import makeFakePrivateChannel from "../../../../__test__/fixures/privateChannel";
+import makeDeletePrivateChannelController from "./delete-privateChannel";
+import makeDeletePrivateChannel from "../use-cases/deletePrivateChannel";
+
+describe("Delete private channel controller", () => {
+    // const channelRequest = {
+    //     body: {},
+    //     headers: {},
+    //     ip: "",
+    //     method: "",
+    //     params: {},
+    //     path: "",
+    //     query: {},
+    // };
+    const handleModeration = async (name: string) => {
+        return await moderateName(name);
+    };
+
+    const privateChannelDb = makePrivateChannelDb({ makeDb });
+    const createChannel = makeCreatePrivateChannel({
+        handleModeration,
+        privateChannelDb,
+    });
+
+    const deletePrivateChannel = makeDeletePrivateChannel({ privateChannelDb });
+    const deletePrivateChannelController = makeDeletePrivateChannelController({
+        deletePrivateChannel,
+    });
+
+    afterEach(async () => {
+        clearDb("private_channels");
+    });
+
+    test("SUCCESS: delete channel", async () => {
+        const channel = await makeFakePrivateChannel(
+            "5c0fc896-1af1-4c26-b917-550ac5eefa9e",
+            "312c0878-04c3-4585-835e-c66900ccc7a1"
+        );
+
+        const channelRequest = {
+            body: { channelId: channel.channelId },
+            headers: {},
+            ip: "",
+            method: "POST",
+            params: {},
+            path: "",
+            query: {},
+        };
+
+        const createdChannel = await createChannel(channel);
+
+        const deletedChannel = await deletePrivateChannelController(
+            channelRequest
+        );
+        expect(deletedChannel.body.data?.channelId).toBe(channel.channelId);
+    });
+
+    test("ERROR: channel id does not exist", async () => {
+        const channel = await makeFakePrivateChannel(
+            "5c0fc896-1af1-4c26-b917-550ac5eefa9e",
+            "312c0878-04c3-4585-835e-c66900ccc7a1"
+        );
+
+        const channelRequest = {
+            body: { channelId: "" },
+            headers: {},
+            ip: "",
+            method: "POST",
+            params: {},
+            path: "",
+            query: {},
+        };
+
+        const createdChannel = await createChannel(channel);
+
+        const deletedChannel = await deletePrivateChannelController(
+            channelRequest
+        );
+        expect(deletedChannel.body.error).toBe(
+            "Channel Id needs to be supplied"
+        );
+    });
+});
