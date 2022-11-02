f84648d890599a80da174d1664a60f64b63a02ac
mrboparai <karanvirboparai123@gmail.com>
2022-11-01 21:44:05 -0400

    created a use case

---
+++ src/Features/privateMessage/use-cases/getPrivateMessageById.ts
@@ -0,0 +1,24 @@
+import { IMakePrivateMessageDb } from "../data-access/privateMessage-db";
+import { IPrivateMessage } from "../privateMessage";
+
+type props = {
+    messageDb: IMakePrivateMessageDb["returnType"];
+};
+
+type returnData = Promise<{
+    success: boolean;
+    data: IPrivateMessage | undefined;
+    error: string;
+}>;
+
+export interface IGetPrivateMessageByIdUseCase {
+    getPrivateMessageById: (messageId: string) => returnData;
+}
+
+export default function makeGetPrivateMessageById({ messageDb }: props) {
+    return async function getPrivateMessageById(messageId: string): returnData {
+        if (!messageId) throw new Error("Message Id needs to be supplied.");
+
+        return messageDb.getPrivateMessageById(messageId);
+    };
+}
