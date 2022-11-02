19de0b96d7ac6cc9a03be6c8e42b0754757c5609
mrboparai <karanvirboparai123@gmail.com>
2022-11-01 21:17:01 -0400

    created index for set up

---
+++ src/Features/privateMessage/db/index.ts
@@ -0,0 +1,28 @@
+import { makeDb } from "../data-access";
+import dotenv from "dotenv";
+dotenv.config();
+
+export default async function setupPrivateMessageDb() {
+    console.log("setting up private message database...");
+    // database will be created if it doesnt exist already
+
+    const db = await makeDb();
+    const result = await db.query(
+        `CREATE TABLE IF NOT EXISTS private_messages (
+          "messageId" VARCHAR(100) PRIMARY KEY,
+          "dateCreated" timestamp,
+          "dateModified" timestamp,
+          "replyTo" VARCHAR(100) references private_messages("messageId"),
+          text VARCHAR(200),
+          "userId" VARCHAR(100) REFERENCES usert("userId"),
+          "privateChannelId" VARCHAR(100) REFERENCES private_channels("channelId") ON DELETE CASCADE
+        );`
+    );
+
+    console.log(
+        "🚀 ~ file: index.ts ~ line 20 ~ setupPrivateMessageDb ~ result",
+        result
+    );
+
+    console.log("Private Message Database set up complete...");
+}
