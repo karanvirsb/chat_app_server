5a69a03ba2d21992d2001e6cf3b45379f6881506
mrboparai <karanvirboparai123@gmail.com>
2022-11-02 09:11:31 -0400

    created test

---
+++ src/Features/privateMessage/use-cases/getPrivateMessageById.spec.ts
@@ -0,0 +1,57 @@
+import makePrivateMessageDb from "../data-access/privateMessage-db";
+import makeDb, { clearDb } from "../../../../__test__/fixures/db";
+import makeCreatePrivateMessage from "./createPrivateMessage";
+import makeFakePrivateMessage from "../../../../__test__/fixures/privateMessage";
+import makePrivateChannelDb from "../../privateChannel/data-access/privateChannel-db";
+import makeGetPrivateMessageById from "./getPrivateMessageById";
+
+describe("Get private messages by id use case", () => {
+    const privateMessageDb = makePrivateMessageDb({ makeDb });
+    const createMessage = makeCreatePrivateMessage({ privateMessageDb });
+    const getMessageById = makeGetPrivateMessageById({ privateMessageDb });
+
+    beforeAll(async () => {
+        const privateChannelDb = makePrivateChannelDb({ makeDb });
+        const found = await privateChannelDb.getPrivateChannelById("123");
+        if (!found.data) {
+            await privateChannelDb.createPrivateChannel({
+                channelId: "123",
+                channelName: "coders",
+                dateCreated: new Date(),
+                friendsId: "5c0fc896-1af1-4c26-b917-550ac5eefa9e",
+                userId: "312c0878-04c3-4585-835e-c66900ccc7a1",
+            });
+        }
+    });
+
+    afterEach(async () => {
+        clearDb("private_messages");
+    });
+
+    test("SUCCESS: getting a message", async () => {
+        const message = await makeFakePrivateMessage(
+            "123",
+            "5c0fc896-1af1-4c26-b917-550ac5eefa9e"
+        );
+        const insertedMessage = await createMessage(message);
+
+        const foundMessage = await getMessageById(message.messageId);
+
+        expect(foundMessage.data?.messageId).toBe(message.messageId);
+    });
+
+    test("ERROR: missing message id ", async () => {
+        const message = await makeFakePrivateMessage(
+            "123",
+            "5c0fc896-1af1-4c26-b917-550ac5eefa9e"
+        );
+        const insertedMessage = await createMessage(message);
+
+        try {
+            const foundMessage = await getMessageById("");
+        } catch (error) {
+            if (error instanceof Error)
+                expect(error.message).toBe("Message Id needs to be supplied.");
+        }
+    });
+});
