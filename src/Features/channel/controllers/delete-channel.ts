b6b19bcf7165fc20c0a3258cc62481f3d6e3fc41
mrboparai <karanvirboparai123@gmail.com>
2022-11-02 11:02:38 -0400

    changed Interface

--- src/Features/channel/controllers/delete-channel.ts
+++ src/Features/channel/controllers/delete-channel.ts
@@ -2,13 +2,13 @@ import {
     IHttpRequest,
     httpResponseType,
 } from "../../../express-callback/index";
-import { IChannel } from "../channel";
+import { IGroupChannel } from "../groupChannel";
 import { IDeleteChannelUseCase } from "../use-cases/deleteChannel";
 
 interface IDeleteChannelResponse extends httpResponseType {
     body: {
         success: boolean;
-        data: IChannel | undefined;
+        data: IGroupChannel | undefined;
         error: string;
     };
 }
