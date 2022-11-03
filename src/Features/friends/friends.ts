da97236125a214379c85c6790cf775bb3ad2c23e
mrboparai <karanvirboparai123@gmail.com>
2022-11-03 09:29:04 -0400

    created a friend feature to add friends

---
+++ src/Features/friends/friends.ts
@@ -0,0 +1,20 @@
+export interface IFriends {
+    userId: string;
+    friendId: string;
+    dateAdded: Date;
+}
+
+export default function buildFriends() {
+    return function makeFriends({ userId, friendId, dateAdded }: IFriends) {
+        if (!userId) throw new Error("User Id needs to be supplied");
+        if (!friendId) throw new Error("Friends Id needs to be supplied");
+        if (!dateAdded || Number.isNaN(dateAdded.getTime()))
+            throw new Error("Date needs to be supplied");
+
+        return Object.freeze({
+            getUserId: () => userId,
+            getFriendsId: () => friendId,
+            getDateAdded: () => dateAdded,
+        });
+    };
+}
