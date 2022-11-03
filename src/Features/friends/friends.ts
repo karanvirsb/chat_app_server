4eaa14aa40dc5927750f47003ab169228d54d643
mrboparai <karanvirboparai123@gmail.com>
2022-11-03 09:42:54 -0400

    updated added date

--- src/Features/friends/friends.ts
+++ src/Features/friends/friends.ts
@@ -5,7 +5,11 @@ export interface IFriends {
 }
 
 export default function buildFriends() {
-    return function makeFriends({ userId, friendId, dateAdded }: IFriends) {
+    return function makeFriends({
+        userId,
+        friendId,
+        dateAdded = new Date(),
+    }: IFriends) {
         if (!userId) throw new Error("User Id needs to be supplied");
         if (!friendId) throw new Error("Friends Id needs to be supplied");
         if (!dateAdded || Number.isNaN(dateAdded.getTime()))
