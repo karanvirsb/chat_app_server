eb0398ae8fe0e94b2a22766a8079e7236a34f4d8
mrboparai <karanvirboparai123@gmail.com>
2022-11-03 10:05:39 -0400

    added a find friends

--- src/Features/friends/data-access/friends-db.ts
+++ src/Features/friends/data-access/friends-db.ts
@@ -11,17 +11,24 @@ type returnData = Promise<{
     error: string;
 }>;
 
+type returnFriends = Promise<{
+    success: boolean;
+    data: undefined | IFriends[];
+    error: string;
+}>;
+
 export interface IMakeFriendsDb {
     returnType: Readonly<{
         addFriend: (friendInfo: IFriends) => returnData;
         deleteFriend: (userId: string, friendId: string) => returnData;
+        getFriends: (userId: string) => returnFriends;
     }>;
 }
 
 export default function makeFriendsDb({
     makeDb,
 }: props): IMakeFriendsDb["returnType"] {
-    return Object.freeze({ addFriend, deleteFriend });
+    return Object.freeze({ addFriend, deleteFriend, getFriends });
 
     async function addFriend(friendInfo: IFriends): returnData {
         const db = await makeDb();
@@ -78,7 +85,39 @@ export default function makeFriendsDb({
             }
         } catch (error) {
             console.log(
-                "🚀 ~ file: friends-db.ts ~ line 39 ~ addFriend ~ error",
+                "🚀 ~ file: friends-db.ts ~ line 87 ~ deleteFriend ~ error",
+                error
+            );
+
+            return {
+                success: false,
+                data: undefined,
+                error: error + "",
+            };
+        } finally {
+            db.release();
+        }
+    }
+
+    async function getFriends(userId: string): returnFriends {
+        const db = await makeDb();
+        try {
+            const query = `SELECT * FROM friends WHERE "userId" = ${userId}`;
+            const res = await db.query(query);
+
+            if (res.rowCount === 1) {
+                const friends: IFriends[] = res.rows;
+                return { success: true, data: friends, error: "" };
+            } else {
+                return {
+                    success: true,
+                    data: undefined,
+                    error: "Could not find friends.",
+                };
+            }
+        } catch (error) {
+            console.log(
+                "🚀 ~ file: friends-db.ts ~ line 119 ~ getFriends ~ error",
                 error
             );
 
