083d82060684c107cb3bd19ea7c057dcf74f2372
mrboparai <karanvirboparai123@gmail.com>
2022-11-04 10:22:23 -0400

    added interface

--- src/Features/friends/use-cases/getAFriend.ts
+++ src/Features/friends/use-cases/getAFriend.ts
@@ -1,10 +1,23 @@
 import { IMakeFriendsDb } from "../data-access/friends-db";
+import { IFriends } from "../friends";
 
 type props = {
     friendsDb: IMakeFriendsDb["returnType"];
 };
 
-export default function makeGetAFriend({ friendsDb }: props) {
+type returnData = Promise<{
+    success: boolean;
+    data: undefined | IFriends;
+    error: string;
+}>;
+
+export interface IGetAFriendUseCase {
+    getAFriend: (userId: string, friendId: string) => returnData;
+}
+
+export default function makeGetAFriend({
+    friendsDb,
+}: props): IGetAFriendUseCase["getAFriend"] {
     return async function getAFriend(userId: string, friendId: string) {
         if (!userId) throw Error("User Id needs to be supplied.");
         if (!friendId) throw Error("Friends Id needs to be supplied.");
