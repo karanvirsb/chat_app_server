f170ac7ce523b80e0dab110b69a3a8589884f749
mrboparai <karanvirboparai123@gmail.com>
2022-11-04 10:21:42 -0400

    added interface

--- src/Features/friends/use-cases/deleteFriend.ts
+++ src/Features/friends/use-cases/deleteFriend.ts
@@ -1,11 +1,24 @@
 import makeFriends from "../index";
 import { IMakeFriendsDb } from "../data-access/friends-db";
+import { IFriends } from "../friends";
 
 type props = {
     friendsDb: IMakeFriendsDb["returnType"];
 };
 
-export default function makeDeleteFriend({ friendsDb }: props) {
+type returnData = Promise<{
+    success: boolean;
+    data: undefined | IFriends;
+    error: string;
+}>;
+
+export interface IDeleteFriendsUseCase {
+    deleteFriend: (userId: string, friendId: string) => returnData;
+}
+
+export default function makeDeleteFriend({
+    friendsDb,
+}: props): IDeleteFriendsUseCase["deleteFriend"] {
     return async function deleteFriend(userId: string, friendId: string) {
         if (!userId) throw Error("User Id needs to be supplied.");
         if (!friendId) throw Error("Friends Id needs to be supplied.");
