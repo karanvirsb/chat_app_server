9055b2a1e3b228682ebcc9b01c09259eecb15838
mrboparai <karanvirboparai123@gmail.com>
2022-11-05 09:17:34 -0400

    added http server

--- server.ts
+++ server.ts
@@ -1,13 +1,16 @@
 const PORT = 8000;
 import app from "./app";
-import setupChannelDb from "./src/Features/groupChannel/db";
+import httpServer from "./sockets";
 
+import setupChannelDb from "./src/Features/groupChannel/db";
 import setUpGroupDb from "./src/Features/group/db";
 import setupMessageDb from "./src/Features/groupMessage/db";
 import setupUserDb from "./src/Features/user/db";
 import setupPrivateChannelDb from "./src/Features/privateChannel/db";
 import setupPrivateMessageDb from "./src/Features/privateMessage/db";
 import setupFriendsDb from "./src/Features/friends/db";
+
+httpServer.listen(8001);
 
 app.listen(PORT, () => {
     console.log(`listening on port: ${PORT}`);
