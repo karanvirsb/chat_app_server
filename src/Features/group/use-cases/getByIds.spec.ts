c863cbcd7bd23293254d3e53632d3c69cd9be648
mrboparai <karanvirboparai123@gmail.com>
2022-12-01 09:39:15 -0500

    added test user and deleted

--- src/Features/group/use-cases/getByIds.spec.ts
+++ src/Features/group/use-cases/getByIds.spec.ts
@@ -1,12 +1,15 @@
 import { moderateName } from "../../../Utilities/moderateText";
 import makeGroupDb from "../data-access/group-db";
-import makeDb, { clearDb } from "../../../../__test__/fixures/db";
+import makeDb, { clearDb, closeDb } from "../../../../__test__/fixures/db";
 import makeFakeGroup from "../../../../__test__/fixures/group";
 import makeAddGroup from "./addGroup";
 import makeGetGroupById from "./getGroupbyId";
 import makeGetGroupByInviteCode from "./getGroupByInviteCode";
 import makeGetUsersByGroupId from "./getUsersByGroupId";
 import { IGroup } from "../group";
+import makeSupertokenDb from "../../../../supertokens/data-access/supertokens-db";
+import makeUsersDb from "../../user/data-access/users-db";
+import supertokens from "../../../../supertokens";
 
 const handleModeration = async (name: string) => {
     return await moderateName(name);
@@ -19,11 +22,42 @@ describe("Get use cases", () => {
     let getGroupByInviteCode = makeGetGroupByInviteCode({ groupDb });
     let getUsersByGroupId = makeGetUsersByGroupId({ groupDb });
 
+    let SupertokensDb = makeSupertokenDb({ makeDb });
     beforeAll(async () => {
-        await clearDb("groupt");
-        await clearDb('"groupUsers"');
+        // creating user if it does not exist
+        const userDb = makeUsersDb({ makeDb });
+        const foundUser = await userDb.findById({
+            id: "cc7d98b5-6f88-4ca5-87e2-435d1546f1fc",
+        });
+
+        // if user does not exist create
+        if (!foundUser.success || !foundUser.data) {
+            const addedUser = await SupertokensDb.addUser({
+                user: {
+                    user_id: "cc7d98b5-6f88-4ca5-87e2-435d1546f1fc",
+                    email: "anTest@gmai.com",
+                    password: "123",
+                    time_joined: Date.now(),
+                },
+            });
+            if (addedUser.success && addedUser.data) {
+                const addUser = await userDb.insert({
+                    data: {
+                        userId: addedUser.data.user_id,
+                        status: "online",
+                        username: "testering",
+                    },
+                });
+            }
+        }
     });
 
+    afterAll(async () => {
+        await clearDb("groupt");
+        await clearDb('"groupUsers"');
+        await supertokens.deleteUser("cc7d98b5-6f88-4ca5-87e2-435d1546f1fc");
+        await closeDb();
+    });
     test("Get group by id", async () => {
         let group = await makeFakeGroup();
         const addedGroup = await addGroup(
