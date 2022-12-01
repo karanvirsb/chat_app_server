f0491becc70503256a5c78f33e2a89254a8d0b9c
mrboparai <karanvirboparai123@gmail.com>
2022-12-01 10:54:06 -0500

    added two test users

--- src/Features/group/controllers/add-userToGroup.spec.ts
+++ src/Features/group/controllers/add-userToGroup.spec.ts
@@ -1,10 +1,13 @@
 import makeGroupDb from "../data-access/group-db";
-import makeDb, { clearDb } from "../../../../__test__/fixures/db";
+import makeDb, { clearDb, closeDb } from "../../../../__test__/fixures/db";
 import makeAddGroup from "../use-cases/addGroup";
 import { moderateName } from "../../../Utilities/moderateText";
 import makeFakeGroup from "../../../../__test__/fixures/group";
 import makeAddUserToGroup from "../use-cases/addUserToGroup";
 import makeAddUserToGroupController from "./add-userToGroup";
+import supertokens from "../../../../supertokens";
+import makeSupertokenDb from "../../../../supertokens/data-access/supertokens-db";
+import makeUsersDb from "../../user/data-access/users-db";
 
 const handleModeration = async (name: string) => {
     return await moderateName(name);
@@ -28,14 +31,60 @@ describe("add user to group controller", () => {
         addUserToGroup,
     });
 
-    beforeEach(async () => {
-        await clearDb("groupt");
-        await clearDb('"groupUsers"');
+    let SupertokensDb = makeSupertokenDb({ makeDb });
+
+    beforeAll(async () => {
+        // creating user if it does not exist
+        const userDb = makeUsersDb({ makeDb });
+        const foundUser = await userDb.findById({
+            id: "cc7d98b5-6f88-4ca5-87e2-435d1546f1fc",
+        });
+
+        // if user does not exist create
+        if (!foundUser.success || !foundUser.data) {
+            const addedUser = await SupertokensDb.addUser({
+                user: {
+                    user_id: "cc7d98b5-6f88-4ca5-87e2-435d1546f1fc",
+                    email: "anTest@gmai.com",
+                    password: "123",
+                    time_joined: Date.now(),
+                },
+            });
+
+            const secondAddedUser = await SupertokensDb.addUser({
+                user: {
+                    user_id: "3443c648-3323-4d6b-8830-c8a1b66a043a",
+                    email: "aTest@gmai.com",
+                    password: "123",
+                    time_joined: Date.now(),
+                },
+            });
+            if (addedUser.data && secondAddedUser.data) {
+                await userDb.insert({
+                    data: {
+                        userId: addedUser.data.user_id,
+                        status: "online",
+                        username: "testering",
+                    },
+                });
+
+                await userDb.insert({
+                    data: {
+                        userId: secondAddedUser.data.user_id,
+                        status: "online",
+                        username: "testings",
+                    },
+                });
+            }
+        }
     });
 
     afterAll(async () => {
         await clearDb("groupt");
         await clearDb('"groupUsers"');
+        await supertokens.deleteUser("cc7d98b5-6f88-4ca5-87e2-435d1546f1fc");
+        await supertokens.deleteUser("3443c648-3323-4d6b-8830-c8a1b66a043a");
+        await closeDb();
     });
 
     test("SUCCESS: added user to the group", async () => {
@@ -58,7 +107,8 @@ describe("add user to group controller", () => {
             "cc7d98b5-6f88-4ca5-87e2-435d1546f1fc"
         );
         const addedUser = await addUserToGroupController(groupRequest);
-        expect(addedUser.body.data?.uId).toBe(
+        console.log(addedUser);
+        expect(addedUser.body.data?.userId).toBe(
             "3443c648-3323-4d6b-8830-c8a1b66a043a"
         );
     });
