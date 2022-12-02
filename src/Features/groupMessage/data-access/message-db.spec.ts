af8ab4ca9103df4106b229a30f11294e4341de9e
mrboparai <karanvirboparai123@gmail.com>
2022-12-02 11:20:55 -0500

    added group test, user tests and channel tests

--- src/Features/groupMessage/data-access/message-db.spec.ts
+++ src/Features/groupMessage/data-access/message-db.spec.ts
@@ -1,22 +1,46 @@
 import makeFakeChannel from "../../../../__test__/fixures/channel";
 import makeDb, { clearDb } from "../../../../__test__/fixures/db";
 import makeFakeMessage from "../../../../__test__/fixures/message";
+import groupTests from "../../../../__test__/functions/group";
+import channelTests from "../../../../__test__/functions/groupChannel";
+import userTests from "../../../../__test__/functions/user";
 import makeMessageDb from "./message-db";
 
 describe("Message db method tests", () => {
     jest.setTimeout(10000);
     const messageDB = makeMessageDb({ makeDb });
 
-    afterEach(async () => {
+    beforeAll(async () => {
+        jest.setTimeout(30000);
+        const addedUser = await userTests.addTestUserToDB({
+            userId: "1234",
+        });
+        const addedGroup = await groupTests.createTestGroup({
+            groupId: "123",
+            userId: "1234",
+        });
+        const addedChannel = await channelTests.createTestChannel({
+            groupId: "123",
+            channelId: "123",
+        });
+    });
+
+    afterAll(async () => {
         await clearDb("group_messages");
+        const deletedChannel = await channelTests.deleteTestChannel({
+            channelId: "123",
+        });
+        const deletedGroup = await groupTests.deleteTestGroup({
+            groupId: "123",
+            userId: "1234",
+        });
+        const deletedUser = await userTests.deleteTestUser({
+            userId: "1234",
+        });
     });
 
     test("SUCCESS: creating a message", async () => {
-        jest.setTimeout(30000);
-        const message = await makeFakeMessage(
-            "123",
-            "5c0fc896-1af1-4c26-b917-550ac5eefa9e"
-        );
+        const message = await makeFakeMessage("123", "1234");
 
         const insertedMessage = await messageDB.createMessage(message);
         expect(insertedMessage.data?.messageId).toBe(message.messageId);
@@ -24,10 +48,7 @@ describe("Message db method tests", () => {
 
     test("SUCCESS: deleting a message", async () => {
         jest.setTimeout(30000);
-        const message = await makeFakeMessage(
-            "123",
-            "5c0fc896-1af1-4c26-b917-550ac5eefa9e"
-        );
+        const message = await makeFakeMessage("123", "1234");
 
         const insertedMessage = await messageDB.createMessage(message);
         const deletedMessage = await messageDB.deleteMessage(message.messageId);
@@ -36,10 +57,7 @@ describe("Message db method tests", () => {
 
     test("SUCCESS: getting message by id", async () => {
         jest.setTimeout(30000);
-        const message = await makeFakeMessage(
-            "123",
-            "5c0fc896-1af1-4c26-b917-550ac5eefa9e"
-        );
+        const message = await makeFakeMessage("123", "1234");
 
         const insertedMessage = await messageDB.createMessage(message);
         const foundMessage = await messageDB.getMessageById(message.messageId);
@@ -48,17 +66,10 @@ describe("Message db method tests", () => {
 
     test("SUCCESS: getting messages by channel id", async () => {
         jest.setTimeout(30000);
-        let message = await makeFakeMessage(
-            "123",
-            "5c0fc896-1af1-4c26-b917-550ac5eefa9e"
-        );
-
+        let message = await makeFakeMessage("123", "1234");
         let insertedMessage = await messageDB.createMessage(message);
         for (let i = 0; i < 10; i++) {
-            message = await makeFakeMessage(
-                "123",
-                "5c0fc896-1af1-4c26-b917-550ac5eefa9e"
-            );
+            message = await makeFakeMessage("123", "1234");
 
             insertedMessage = await messageDB.createMessage(message);
         }
@@ -68,16 +79,13 @@ describe("Message db method tests", () => {
             10
         );
         console.log(foundMessages);
-        if (foundMessages.data)
-            expect(foundMessages.data[0].text).toBe(message.text);
+        if (foundMessages.data && insertedMessage.data)
+            expect(foundMessages.data[1].text).toBe(insertedMessage.data.text);
     });
 
     test("SUCCESS: updating message", async () => {
         jest.setTimeout(30000);
-        const message = await makeFakeMessage(
-            "123",
-            "5c0fc896-1af1-4c26-b917-550ac5eefa9e"
-        );
+        const message = await makeFakeMessage("123", "1234");
 
         const insertedMessage = await messageDB.createMessage(message);
         const updatedMessage = await messageDB.updateMessage(
@@ -90,10 +98,7 @@ describe("Message db method tests", () => {
 
     test("SUCCESS: updating message date", async () => {
         jest.setTimeout(30000);
-        const message = await makeFakeMessage(
-            "123",
-            "5c0fc896-1af1-4c26-b917-550ac5eefa9e"
-        );
+        const message = await makeFakeMessage("123", "1234");
 
         const insertedMessage = await messageDB.createMessage(message);
         const updatedMessage = await messageDB.updateMessage(
@@ -101,7 +106,7 @@ describe("Message db method tests", () => {
             message.messageId,
             `to_timestamp(${new Date().getTime() / 1000})`
         );
-        console.log(updatedMessage);
+
         expect(updatedMessage.data?.dateModified).not.toEqual(
             message.dateModified
         );
