a4aa14b8803474303aaa0808c3b7a133fea77b74
mrboparai <karanvirboparai123@gmail.com>
2022-12-02 10:29:16 -0500

    updated delete users decoupled

--- src/Features/groupChannel/data-access/channel-db.spec.ts
+++ src/Features/groupChannel/data-access/channel-db.spec.ts
@@ -1,6 +1,7 @@
 import makeFakeChannel from "../../../../__test__/fixures/channel";
 import makeDb, { clearDb } from "../../../../__test__/fixures/db";
 import groupTests from "../../../../__test__/functions/group";
+import userTests from "../../../../__test__/functions/user";
 import makeChannelDb from "./channel-db";
 
 describe("Channel db method tests", () => {
@@ -9,6 +10,7 @@ describe("Channel db method tests", () => {
 
     beforeAll(async () => {
         jest.setTimeout(30000);
+        const addedUser = await userTests.addTestUserToDB({ userId: "123" });
         const addedGroup = await groupTests.createTestGroup({
             groupId: "123",
             userId: "123",
@@ -17,6 +19,7 @@ describe("Channel db method tests", () => {
 
     afterAll(async () => {
         await clearDb("group_channels");
+        const deletedUser = await userTests.deleteTestUser({ userId: "123" });
         const deletedGroup = await groupTests.deleteTestGroup({
             groupId: "123",
             userId: "123",
