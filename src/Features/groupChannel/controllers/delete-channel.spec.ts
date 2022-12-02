023b6bf852d2e289d4abb11e033b79c51f06f389
mrboparai <karanvirboparai123@gmail.com>
2022-12-02 10:32:04 -0500

    updated with user tests

--- src/Features/groupChannel/controllers/delete-channel.spec.ts
+++ src/Features/groupChannel/controllers/delete-channel.spec.ts
@@ -6,6 +6,7 @@ import makeFakeChannel from "../../../../__test__/fixures/channel";
 import makeDeleteChannel from "../use-cases/deleteChannel";
 import makeDeleteChannelController from "./delete-channel";
 import groupTests from "../../../../__test__/functions/group";
+import userTests from "../../../../__test__/functions/user";
 
 describe("Delete channel controller", () => {
     // const channelRequest = {
@@ -31,6 +32,7 @@ describe("Delete channel controller", () => {
 
     beforeAll(async () => {
         jest.setTimeout(30000);
+        const addedUser = await userTests.addTestUserToDB({ userId: "123" });
         const addedGroup = await groupTests.createTestGroup({
             groupId: "123",
             userId: "123",
@@ -39,6 +41,7 @@ describe("Delete channel controller", () => {
 
     afterAll(async () => {
         await clearDb("group_channels");
+        const deletedUser = await userTests.deleteTestUser({ userId: "123" });
         const deletedGroup = await groupTests.deleteTestGroup({
             groupId: "123",
             userId: "123",
