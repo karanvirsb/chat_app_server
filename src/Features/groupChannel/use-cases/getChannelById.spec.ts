e1ac3a53108efc5ce5aa53e6eeca72a254a9656d
mrboparai <karanvirboparai123@gmail.com>
2022-12-02 10:05:33 -0500

    added group tests

--- src/Features/groupChannel/use-cases/getChannelById.spec.ts
+++ src/Features/groupChannel/use-cases/getChannelById.spec.ts
@@ -4,6 +4,7 @@ import makeCreateChannel, { handleModerationType } from "./createChannel";
 import { moderateName } from "../../../Utilities/moderateText";
 import makeFakeChannel from "../../../../__test__/fixures/channel";
 import makeGetChannelById from "./getChannelById";
+import groupTests from "../../../../__test__/functions/group";
 
 describe("get channel by id use case", () => {
     const handleModeration: handleModerationType = async (
@@ -20,8 +21,20 @@ describe("get channel by id use case", () => {
         channelDb,
     });
 
-    afterEach(async () => {
-        clearDb("group_channels");
+    beforeAll(async () => {
+        jest.setTimeout(30000);
+        const addedGroup = await groupTests.createTestGroup({
+            groupId: "123",
+            userId: "123",
+        });
+    });
+
+    afterAll(async () => {
+        await clearDb("group_channels");
+        const deletedGroup = await groupTests.deleteTestGroup({
+            groupId: "123",
+            userId: "123",
+        });
     });
 
     test("SUCCESS: get channel by id", async () => {
