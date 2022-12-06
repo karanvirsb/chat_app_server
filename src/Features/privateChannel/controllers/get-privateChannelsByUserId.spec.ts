7fe0f4ac70c6c836ebc12942921e81e4a6cf2e0d
mrboparai <karanvirboparai123@gmail.com>
2022-12-06 10:31:26 -0500

    added fake user tests

--- src/Features/privateChannel/controllers/get-privateChannelsByUserId.spec.ts
+++ src/Features/privateChannel/controllers/get-privateChannelsByUserId.spec.ts
@@ -5,6 +5,7 @@ import { moderateName } from "../../../Utilities/moderateText";
 import makeFakePrivateChannel from "../../../../__test__/fixures/privateChannel";
 import makeGetPrivateChannelsByUserId from "../use-cases/getPrivateChannelsByUserId";
 import makeGetPrivateChannelsByUserIdController from "./get-privateChannelsByUserId";
+import userTests from "../../../../__test__/functions/user";
 
 describe("Get private channels by group id controller", () => {
     // const channelRequest = {
@@ -34,8 +35,25 @@ describe("Get private channels by group id controller", () => {
             getPrivateChannelsByUserId,
         });
 
-    afterEach(async () => {
-        clearDb("private_channels");
+    beforeAll(async () => {
+        jest.setTimeout(30000);
+        const addedUser = await userTests.addTestUserToDB({
+            userId: "5c0fc896-1af1-4c26-b917-550ac5eefa9e",
+        });
+        const secondUser = await userTests.addTestUserToDB({
+            userId: "312c0878-04c3-4585-835e-c66900ccc7a1",
+        });
+    });
+
+    afterAll(async () => {
+        jest.setTimeout(30000);
+        await clearDb("private_channels");
+        const deletedUser = await userTests.deleteTestUser({
+            userId: "5c0fc896-1af1-4c26-b917-550ac5eefa9e",
+        });
+        const deletedSecondUser = await userTests.deleteTestUser({
+            userId: "312c0878-04c3-4585-835e-c66900ccc7a1",
+        });
     });
 
     test("SUCCESS: get channel by user id", async () => {
