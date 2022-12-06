29cf45a2e6485e5aff0c9283998b79af3d791b91
mrboparai <karanvirboparai123@gmail.com>
2022-12-06 10:13:58 -0500

    added fake data

--- src/Features/groupMessage/controllers/update-messageText.spec.ts
+++ src/Features/groupMessage/controllers/update-messageText.spec.ts
@@ -1,5 +1,8 @@
 import makeDb, { clearDb } from "../../../../__test__/fixures/db";
 import makeFakeMessage from "../../../../__test__/fixures/message";
+import groupTests from "../../../../__test__/functions/group";
+import channelTests from "../../../../__test__/functions/groupChannel";
+import userTests from "../../../../__test__/functions/user";
 import makeMessageDb from "../data-access/message-db";
 import makeCreateMessage from "../use-cases/createMessage";
 import makeUpdateDateModified from "../use-cases/updateDateModified";
@@ -16,10 +19,34 @@ describe("updating message text controller", () => {
         updateMessageText,
     });
 
-    afterEach(async () => {
-        await clearDb("group_messages");
+    beforeAll(async () => {
+        jest.setTimeout(30000);
+        const addedUser = await userTests.addTestUserToDB({
+            userId: "5c0fc896-1af1-4c26-b917-550ac5eefa9e",
+        });
+        const addedGroup = await groupTests.createTestGroup({
+            groupId: "123",
+            userId: "5c0fc896-1af1-4c26-b917-550ac5eefa9e",
+        });
+        const addedChannel = await channelTests.createTestChannel({
+            groupId: "123",
+            channelId: "123",
+        });
     });
 
+    afterAll(async () => {
+        await clearDb("group_messages");
+        const deletedChannel = await channelTests.deleteTestChannel({
+            channelId: "123",
+        });
+        const deletedGroup = await groupTests.deleteTestGroup({
+            groupId: "123",
+            userId: "5c0fc896-1af1-4c26-b917-550ac5eefa9e",
+        });
+        const deletedUser = await userTests.deleteTestUser({
+            userId: "5c0fc896-1af1-4c26-b917-550ac5eefa9e",
+        });
+    });
     test("SUCCESS: updated message text", async () => {
         const message = await makeFakeMessage(
             "123",
