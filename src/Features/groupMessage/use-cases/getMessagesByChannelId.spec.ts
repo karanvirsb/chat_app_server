18ce31644c9d9c02c17b47e9c5c3e9d7f7dc08f9
mrboparai <karanvirboparai123@gmail.com>
2022-12-06 10:00:49 -0500

    added tests

--- src/Features/groupMessage/use-cases/getMessagesByChannelId.spec.ts
+++ src/Features/groupMessage/use-cases/getMessagesByChannelId.spec.ts
@@ -3,14 +3,42 @@ import makeDb, { clearDb } from "../../../../__test__/fixures/db";
 import makeCreateMessage from "./createMessage";
 import makeFakeMessage from "../../../../__test__/fixures/message";
 import makeGetMessagesByChannelId from "./getMessagesByChannelId";
+import userTests from "../../../../__test__/functions/user";
+import groupTests from "../../../../__test__/functions/group";
+import channelTests from "../../../../__test__/functions/groupChannel";
 
 describe("Getting messages by channel id use case", () => {
     const messageDb = makeMessageDb({ makeDb });
     const createMessage = makeCreateMessage({ messageDb });
     const getMessagesByChannelId = makeGetMessagesByChannelId({ messageDb });
 
-    afterEach(async () => {
-        clearDb("group_messages");
+    beforeAll(async () => {
+        jest.setTimeout(30000);
+        const addedUser = await userTests.addTestUserToDB({
+            userId: "5c0fc896-1af1-4c26-b917-550ac5eefa9e",
+        });
+        const addedGroup = await groupTests.createTestGroup({
+            groupId: "123",
+            userId: "5c0fc896-1af1-4c26-b917-550ac5eefa9e",
+        });
+        const addedChannel = await channelTests.createTestChannel({
+            groupId: "123",
+            channelId: "123",
+        });
+    });
+
+    afterAll(async () => {
+        await clearDb("group_messages");
+        const deletedChannel = await channelTests.deleteTestChannel({
+            channelId: "123",
+        });
+        const deletedGroup = await groupTests.deleteTestGroup({
+            groupId: "123",
+            userId: "5c0fc896-1af1-4c26-b917-550ac5eefa9e",
+        });
+        const deletedUser = await userTests.deleteTestUser({
+            userId: "5c0fc896-1af1-4c26-b917-550ac5eefa9e",
+        });
     });
 
     test("SUCCESS: getting a message", async () => {
