8bd67f85b571c191285c0135cc1257f65f231915
mrboparai <karanvirboparai123@gmail.com>
2022-12-06 10:13:23 -0500

    added fake data

--- src/Features/groupMessage/controllers/create-message.spec.ts
+++ src/Features/groupMessage/controllers/create-message.spec.ts
@@ -1,5 +1,8 @@
 import makeDb, { clearDb } from "../../../../__test__/fixures/db";
 import makeFakeMessage from "../../../../__test__/fixures/message";
+import groupTests from "../../../../__test__/functions/group";
+import channelTests from "../../../../__test__/functions/groupChannel";
+import userTests from "../../../../__test__/functions/user";
 import makeMessageDb from "../data-access/message-db";
 import makeCreateMessage from "../use-cases/createMessage";
 import makeCreateMessageController from "./create-message";
@@ -11,8 +14,33 @@ describe("creating a message controller", () => {
         createMessage,
     });
 
-    afterEach(async () => {
+    beforeAll(async () => {
+        jest.setTimeout(30000);
+        const addedUser = await userTests.addTestUserToDB({
+            userId: "5c0fc896-1af1-4c26-b917-550ac5eefa9e",
+        });
+        const addedGroup = await groupTests.createTestGroup({
+            groupId: "123",
+            userId: "5c0fc896-1af1-4c26-b917-550ac5eefa9e",
+        });
+        const addedChannel = await channelTests.createTestChannel({
+            groupId: "123",
+            channelId: "123",
+        });
+    });
+
+    afterAll(async () => {
         await clearDb("group_messages");
+        const deletedChannel = await channelTests.deleteTestChannel({
+            channelId: "123",
+        });
+        const deletedGroup = await groupTests.deleteTestGroup({
+            groupId: "123",
+            userId: "5c0fc896-1af1-4c26-b917-550ac5eefa9e",
+        });
+        const deletedUser = await userTests.deleteTestUser({
+            userId: "5c0fc896-1af1-4c26-b917-550ac5eefa9e",
+        });
     });
 
     test("SUCCESS: create a message", async () => {
