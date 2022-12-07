6095a928e5723bfbabb3cbb4ca0a91e9b068375e
mrboparai <karanvirboparai123@gmail.com>
2022-12-06 20:19:14 -0500

    updated with fake user data

--- src/Features/friends/use-cases/getFriends.spec.ts
+++ src/Features/friends/use-cases/getFriends.spec.ts
@@ -1,6 +1,7 @@
 import { getFriends } from ".";
 import makeDb, { clearDb } from "../../../../__test__/fixures/db";
 import makeFakeFriends from "../../../../__test__/fixures/friends";
+import userTests from "../../../../__test__/functions/user";
 import makeFriendsDb from "../data-access/friends-db";
 import makeAddFriend from "./addFriend";
 import makeGetFriends from "./getFriends";
@@ -10,8 +11,33 @@ describe("Getting friends use case", () => {
     const addFriend = makeAddFriend({ friendsDb });
     const getFriends = makeGetFriends({ friendsDb });
 
+    jest.setTimeout(30000);
+    const users = [
+        "5c0fc896-1af1-4c26-b917-550ac5eefa9e",
+        "312c0878-04c3-4585-835e-c66900ccc7a1",
+        "cc7d98b5-6f88-4ca5-87e2-435d1546f1fc",
+    ];
+    beforeAll(async () => {
+        const fakeUser = await userTests.addTestUserToDB({ userId: users[0] });
+        const fakeUser1 = await userTests.addTestUserToDB({ userId: users[1] });
+        const fakeUser2 = await userTests.addTestUserToDB({ userId: users[2] });
+    });
+
     afterEach(async () => {
         await clearDb("friends");
+    });
+    afterAll(async () => {
+        await clearDb("friends");
+
+        const deletedFakeUser = await userTests.addTestUserToDB({
+            userId: users[0],
+        });
+        const deletedFakeUser1 = await userTests.addTestUserToDB({
+            userId: users[1],
+        });
+        const deletedFakeUser2 = await userTests.addTestUserToDB({
+            userId: users[2],
+        });
     });
 
     test("SUCCESS: getting friends", async () => {
