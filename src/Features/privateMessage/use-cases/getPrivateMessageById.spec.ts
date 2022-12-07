671a6a5641baf9f2c6cd07fcba77666dd100208d
mrboparai <karanvirboparai123@gmail.com>
2022-12-06 19:55:00 -0500

    added fake data tests

--- src/Features/privateMessage/use-cases/getPrivateMessageById.spec.ts
+++ src/Features/privateMessage/use-cases/getPrivateMessageById.spec.ts
@@ -4,30 +4,43 @@ import makeCreatePrivateMessage from "./createPrivateMessage";
 import makeFakePrivateMessage from "../../../../__test__/fixures/privateMessage";
 import makePrivateChannelDb from "../../privateChannel/data-access/privateChannel-db";
 import makeGetPrivateMessageById from "./getPrivateMessageById";
+import privateChannelTests from "../../../../__test__/functions/privateChannel";
+import userTests from "../../../../__test__/functions/user";
 
 describe("Get private messages by id use case", () => {
     const privateMessageDb = makePrivateMessageDb({ makeDb });
     const createMessage = makeCreatePrivateMessage({ privateMessageDb });
     const getMessageById = makeGetPrivateMessageById({ privateMessageDb });
 
+    jest.setTimeout(30000);
     beforeAll(async () => {
-        const privateChannelDb = makePrivateChannelDb({ makeDb });
-        const found = await privateChannelDb.getPrivateChannelById("123");
-        if (!found.data) {
-            await privateChannelDb.createPrivateChannel({
+        const addedUser = await userTests.addTestUserToDB({
+            userId: "5c0fc896-1af1-4c26-b917-550ac5eefa9e",
+        });
+        const secondUser = await userTests.addTestUserToDB({
+            userId: "312c0878-04c3-4585-835e-c66900ccc7a1",
+        });
+        const privateChannel =
+            await privateChannelTests.createTestPrivateChannel({
+                userId: "5c0fc896-1af1-4c26-b917-550ac5eefa9e",
+                friendsId: "312c0878-04c3-4585-835e-c66900ccc7a1",
                 channelId: "123",
-                channelName: "coders",
-                dateCreated: new Date(),
-                friendsId: "5c0fc896-1af1-4c26-b917-550ac5eefa9e",
-                userId: "312c0878-04c3-4585-835e-c66900ccc7a1",
             });
-        }
     });
 
-    afterEach(async () => {
-        clearDb("private_messages");
+    afterAll(async () => {
+        await clearDb("group_messages");
+        const deletedPrivateChannel =
+            await privateChannelTests.deleteTestPrivateChannel({
+                channelId: "123",
+            });
+        const deletedUser = await userTests.deleteTestUser({
+            userId: "5c0fc896-1af1-4c26-b917-550ac5eefa9e",
+        });
+        const deletedSecondUser = await userTests.deleteTestUser({
+            userId: "312c0878-04c3-4585-835e-c66900ccc7a1",
+        });
     });
-
     test("SUCCESS: getting a message", async () => {
         const message = await makeFakePrivateMessage(
             "123",
