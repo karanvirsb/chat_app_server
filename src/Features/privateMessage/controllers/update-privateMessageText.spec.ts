f202c123cea03952e378c2b6695f5342eb3759db
mrboparai <karanvirboparai123@gmail.com>
2022-12-06 19:59:53 -0500

    updated with fake tests data

--- src/Features/privateMessage/controllers/update-privateMessageText.spec.ts
+++ src/Features/privateMessage/controllers/update-privateMessageText.spec.ts
@@ -1,5 +1,7 @@
 import makeDb, { clearDb } from "../../../../__test__/fixures/db";
 import makeFakePrivateMessage from "../../../../__test__/fixures/privateMessage";
+import privateChannelTests from "../../../../__test__/functions/privateChannel";
+import userTests from "../../../../__test__/functions/user";
 import makePrivateChannelDb from "../../privateChannel/data-access/privateChannel-db";
 import makePrivateMessageDb from "../data-access/privateMessage-db";
 import makeCreatePrivateMessage from "../use-cases/createPrivateMessage";
@@ -18,22 +20,34 @@ describe("updating private message text controller", () => {
             updatePrivateMessageText,
         });
 
+    jest.setTimeout(30000);
     beforeAll(async () => {
-        const privateChannelDb = makePrivateChannelDb({ makeDb });
-        const found = await privateChannelDb.getPrivateChannelById("123");
-        if (!found.data) {
-            await privateChannelDb.createPrivateChannel({
+        const addedUser = await userTests.addTestUserToDB({
+            userId: "5c0fc896-1af1-4c26-b917-550ac5eefa9e",
+        });
+        const secondUser = await userTests.addTestUserToDB({
+            userId: "312c0878-04c3-4585-835e-c66900ccc7a1",
+        });
+        const privateChannel =
+            await privateChannelTests.createTestPrivateChannel({
+                userId: "5c0fc896-1af1-4c26-b917-550ac5eefa9e",
+                friendsId: "312c0878-04c3-4585-835e-c66900ccc7a1",
                 channelId: "123",
-                channelName: "coders",
-                dateCreated: new Date(),
-                friendsId: "5c0fc896-1af1-4c26-b917-550ac5eefa9e",
-                userId: "312c0878-04c3-4585-835e-c66900ccc7a1",
             });
-        }
     });
 
-    afterEach(async () => {
-        await clearDb("private_messages");
+    afterAll(async () => {
+        await clearDb("group_messages");
+        const deletedPrivateChannel =
+            await privateChannelTests.deleteTestPrivateChannel({
+                channelId: "123",
+            });
+        const deletedUser = await userTests.deleteTestUser({
+            userId: "5c0fc896-1af1-4c26-b917-550ac5eefa9e",
+        });
+        const deletedSecondUser = await userTests.deleteTestUser({
+            userId: "312c0878-04c3-4585-835e-c66900ccc7a1",
+        });
     });
 
     test("SUCCESS: updated message text", async () => {
