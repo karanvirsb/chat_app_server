7a0a677747411fcbe1dd203d7a780a83ca8e97ae
mrboparai <karanvirboparai123@gmail.com>
2022-12-08 09:47:46 -0500

    fixed failing tests by adding fake user data and better error catching

--- src/Features/user/use-cases/addUser.spec.ts
+++ src/Features/user/use-cases/addUser.spec.ts
@@ -1,8 +1,11 @@
 import makeAddUser from "./addUser";
 import { moderateName } from "../../../Utilities/moderateText";
-import makeDb, { clearDb } from "../../../../__test__/fixures/db";
+import makeDb, { clearDb, closeDb } from "../../../../__test__/fixures/db";
 import makeUsersDb from "../data-access/users-db";
 import makeFakeUser from "../../../../__test__/fixures/user";
+import makeSupertokenDb, {
+    IMakeSupertokensDb,
+} from "../../../../supertokens/data-access/supertokens-db";
 
 const handleModeration = async (name: string) => {
     return await moderateName(name);
@@ -12,12 +15,37 @@ describe("Add User case", () => {
     let usersDb = makeUsersDb({ makeDb });
     let addUser = makeAddUser({ usersDb, handleModeration });
 
-    afterAll(() => {
-        clearDb("usert");
+    let SupertokensDb: IMakeSupertokensDb["returnType"] = makeSupertokenDb({
+        makeDb,
     });
 
+    beforeAll(async () => {
+        const createdUser = await SupertokensDb.addUser({
+            user: {
+                user_id: "12345678910",
+                email: "random@gmai.com",
+                password: "123",
+                time_joined: Date.now(),
+            },
+        });
+        usersDb = makeUsersDb({ makeDb });
+        await clearDb("usert");
+    });
+
+    afterEach(async () => {
+        await clearDb("usert");
+    });
+
+    afterAll(async () => {
+        const deletedUser = await SupertokensDb.deleteUser({
+            userId: "12345678910",
+        });
+        await closeDb();
+    });
+
+    jest.setTimeout(30000);
     it("User added successfully", async () => {
-        const user = await makeFakeUser();
+        const user = await makeFakeUser({ userId: "12345678910" });
         const resp = await addUser(user);
         if (resp.success) {
             expect(resp.data?.username).toBe(user.username);
@@ -25,21 +53,25 @@ describe("Add User case", () => {
     });
 
     it("Duplicate User", async () => {
-        const user = await makeFakeUser();
+        const user = await makeFakeUser({ userId: "12345678910" });
         const resp = await addUser(user);
-        if (resp.success) {
+        try {
             const err = await addUser(user);
-            if (err.success) expect(err.error).toBe("User already exists");
+        } catch (error) {
+            if (error instanceof Error)
+                expect(error.message).toBe("User already exists");
         }
     });
 
     it("Moderated username", async () => {
-        const user = await makeFakeUser();
+        const user = await makeFakeUser({ userId: "12345678910" });
         user["username"] = "bullshit";
 
-        const resp = await addUser(user);
-        if (!resp.success) {
-            expect(resp.error).toBe("Username contains profanity");
+        try {
+            const resp = await addUser(user);
+        } catch (error) {
+            if (error instanceof Error)
+                expect(error.message).toBe("Username contains profanity");
         }
     });
 });
