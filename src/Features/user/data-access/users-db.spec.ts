b9d42083b375b745ea5f8ed00f17804412339bf9
mrboparai <karanvirboparai123@gmail.com>
2022-12-08 09:41:21 -0500

    updated with fake user tests and updating inputs

--- src/Features/user/data-access/users-db.spec.ts
+++ src/Features/user/data-access/users-db.spec.ts
@@ -1,11 +1,25 @@
 import makeDb, { clearDb, closeDb } from "../../../../__test__/fixures/db";
 import makeUsersDb, { IMakeUsersDb } from "./users-db";
 import makeFakerUser from "../../../../__test__/fixures/user";
+import makeSupertokenDb, {
+    IMakeSupertokensDb,
+} from "../../../../supertokens/data-access/supertokens-db";
 
 describe("Users DB", () => {
     let UsersDb: IMakeUsersDb["returnType"];
+    let SupertokensDb: IMakeSupertokensDb["returnType"] = makeSupertokenDb({
+        makeDb,
+    });
 
     beforeAll(async () => {
+        const createdUser = await SupertokensDb.addUser({
+            user: {
+                user_id: "1234",
+                email: "random@gmai.com",
+                password: "123",
+                time_joined: Date.now(),
+            },
+        });
         UsersDb = makeUsersDb({ makeDb });
         await clearDb("usert");
     });
@@ -15,13 +29,12 @@ describe("Users DB", () => {
     });
 
     afterAll(async () => {
-        await clearDb("usert");
+        const deletedUser = await SupertokensDb.deleteUser({ userId: "1234" });
         await closeDb();
     });
-
     it("Insert a user", async () => {
         // successful
-        const user = await makeFakerUser();
+        const user = await makeFakerUser({ userId: "1234" });
         const resp = await UsersDb.insert({ data: user });
 
         if (resp.success) {
@@ -30,7 +43,7 @@ describe("Users DB", () => {
     });
     it("Find user by Id", async () => {
         //successful
-        const fakeUser = await makeFakerUser();
+        const fakeUser = await makeFakerUser({ userId: "1234" });
         const insertedUser = await UsersDb.insert({ data: fakeUser });
         const resp = await UsersDb.findById({ id: fakeUser.userId });
         if (resp.success) {
@@ -40,7 +53,7 @@ describe("Users DB", () => {
 
     it("Find user by username", async () => {
         // success
-        const fakeUser = await makeFakerUser();
+        const fakeUser = await makeFakerUser({ userId: "1234" });
         const insertedUser = await UsersDb.insert({ data: fakeUser });
         const resp = await UsersDb.findByUsername(fakeUser.username);
         if (resp.success) {
@@ -49,7 +62,7 @@ describe("Users DB", () => {
     });
 
     it("Remove user by Id", async () => {
-        const fakeUser = await makeFakerUser();
+        const fakeUser = await makeFakerUser({ userId: "1234" });
         const insertedUser = await UsersDb.insert({ data: fakeUser });
         const resp = await UsersDb.remove(fakeUser.userId);
         if (resp.success) {
@@ -58,7 +71,7 @@ describe("Users DB", () => {
     });
 
     it("Update user by username", async () => {
-        const fakeUser = await makeFakerUser();
+        const fakeUser = await makeFakerUser({ userId: "1234" });
         const insertedUser = await UsersDb.insert({ data: fakeUser });
         const resp = await UsersDb.updateByUsername({
             username: fakeUser.username,
@@ -72,7 +85,7 @@ describe("Users DB", () => {
     });
 
     it("Update user by user id", async () => {
-        const fakeUser = await makeFakerUser();
+        const fakeUser = await makeFakerUser({ userId: "1234" });
         const insertedUser = await UsersDb.insert({ data: fakeUser });
         const resp = await UsersDb.updateByUserId({
             userId: fakeUser.userId,
