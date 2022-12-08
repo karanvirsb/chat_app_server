16b00d9e689596a20a28df7e282a68388f52ad55
mrboparai <karanvirboparai123@gmail.com>
2022-12-08 10:45:04 -0500

    fixed userId

--- src/Features/group/use-cases/addGroup.spec.ts
+++ src/Features/group/use-cases/addGroup.spec.ts
@@ -48,7 +48,9 @@ describe("Adding group use case", () => {
     afterAll(async () => {
         await clearDb("groupt");
         await clearDb('"groupUsers"');
-        await supertokens.deleteUser("cc7d98b5-6f88-4ca5-87e2-435d1546f1fc");
+        await SupertokensDb.deleteUser({
+            userId: "cc7d98b5-6f88-4ca5-87e2-435d1546f1fc",
+        });
         await closeDb();
     });
 
