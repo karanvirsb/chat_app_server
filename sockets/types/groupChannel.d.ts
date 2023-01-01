d29ef76750841a37c8dbb046d5b3b866c0c38350
karanvirsb <karanvirboparai123@gmail.com>
2023-01-01 11:51:04 -0500

    updated and added group channel socket

--- sockets/types/groupChannel.d.ts
+++ sockets/types/groupChannel.d.ts
@@ -1,37 +1,4 @@
-import { IGroup } from "../src/Features/group/group";
-import { IUser } from "../src/Features/user/user";
-
-export type InvalidateEvent = {
-  queryTags: string[];
+export type UpdateChannelsListEvent = {
   groupId: string;
-};
-
-export type UpdateEvent = {
-  groupId: string;
-  payload: Partial<IGroup>;
-};
-
-export type DeleteEvent = {
-  groupId: string;
-  payload: {};
-};
-
-export type JoinRoomEvent = {
-  rooms: string[];
-  userId: string;
-};
-
-export type UpdateGroupUsersEvent = {
-  groupId: string;
-  payload: { userInfo: IUser };
-};
-
-export type LeaveRoomEvent = {
-  groupId: string;
-  payload: { userId: string };
-};
-
-export type LeaveGroupEvent = {
-  groupId: string;
-  payload: { userId: string };
+  payload: { channelInfo: IGroupChannel };
 };
