a9867f33876d6ba471555a1e7bf5ce40349a0dc9
karanvirsb <karanvirboparai123@gmail.com>
2022-12-31 20:01:06 -0500

    added awaits

--- server.ts
+++ server.ts
@@ -7,20 +7,33 @@ import setupUserDb from "./src/Features/user/db";
 import setupPrivateChannelDb from "./src/Features/privateChannel/db";
 import setupPrivateMessageDb from "./src/Features/privateMessage/db";
 import setupFriendsDb from "./src/Features/friends/db";
+import sleep from "./src/Utilities/sleep";
 
-httpServer.listen(PORT, () => {
-    console.log(`listening on port: ${PORT}`);
-    try {
-        setupUserDb();
-        setUpGroupDb();
-        setupChannelDb();
-        setupPrivateChannelDb();
-        setupMessageDb();
-        setupPrivateMessageDb();
-        setupFriendsDb();
-    } catch (error) {
-        console.log(error);
-    }
+httpServer.listen(PORT, async () => {
+  console.log(`listening on port: ${PORT}`);
+  try {
+    await setupUserDb();
+    await sleep(1000);
+
+    await setUpGroupDb();
+    await sleep(1000);
+
+    await setupChannelDb();
+    await sleep(1000);
+
+    await setupPrivateChannelDb();
+    await sleep(1000);
+
+    await setupMessageDb();
+    await sleep(1000);
+
+    await setupPrivateMessageDb();
+    await sleep(1000);
+
+    await setupFriendsDb();
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 // app.listen(PORT, () => {
