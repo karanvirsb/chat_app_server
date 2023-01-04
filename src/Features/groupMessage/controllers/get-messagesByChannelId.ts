27cf34777e2e2029f61e3277c24f4d2014b1d23b
karanvirsb <karanvirboparai123@gmail.com>
2023-01-04 17:10:31 -0500

    updated messageId to channel id

--- src/Features/groupMessage/controllers/get-messagesByChannelId.ts
+++ src/Features/groupMessage/controllers/get-messagesByChannelId.ts
@@ -1,49 +1,49 @@
 import {
-    IHttpRequest,
-    httpResponseType,
+  IHttpRequest,
+  httpResponseType,
 } from "../../../express-callback/index";
 import { IGroupMessage } from "../groupMessage";
 import { IGetMessagesByChannelIdUseCase } from "../use-cases/getMessagesByChannelId";
 
 interface IGetMessagesByChannelIdResponse extends httpResponseType {
-    body: {
-        success: boolean;
-        data: IGroupMessage[] | undefined;
-        error: string;
-    };
+  body: {
+    success: boolean;
+    data: IGroupMessage[] | undefined;
+    error: string;
+  };
 }
 
 export default function makeGetMessagesByChannelIdController({
-    getMessagesByChannelId,
+  getMessagesByChannelId,
 }: IGetMessagesByChannelIdUseCase) {
-    return async function getMessagesByChannelIdController(
-        httpRequest: IHttpRequest
-    ): Promise<IGetMessagesByChannelIdResponse> {
-        const headers: { [key: string]: string } = {
-            "Content-Type": "application/json",
-        };
-        try {
-            const foundMessages = await getMessagesByChannelId(
-                httpRequest.params.messageId,
-                new Date(httpRequest.params.dateCreated),
-                parseInt(httpRequest.params.limit)
-            );
-            return {
-                headers,
-                statusCode: 200,
-                body: foundMessages,
-            };
-        } catch (error: any) {
-            console.log(error);
-            return {
-                headers,
-                statusCode: 400,
-                body: {
-                    success: false,
-                    data: undefined,
-                    error: error.message,
-                },
-            };
-        }
+  return async function getMessagesByChannelIdController(
+    httpRequest: IHttpRequest
+  ): Promise<IGetMessagesByChannelIdResponse> {
+    const headers: { [key: string]: string } = {
+      "Content-Type": "application/json",
     };
+    try {
+      const foundMessages = await getMessagesByChannelId(
+        httpRequest.params.channelId,
+        new Date(httpRequest.params.dateCreated),
+        parseInt(httpRequest.params.limit)
+      );
+      return {
+        headers,
+        statusCode: 200,
+        body: foundMessages,
+      };
+    } catch (error: any) {
+      console.log(error);
+      return {
+        headers,
+        statusCode: 400,
+        body: {
+          success: false,
+          data: undefined,
+          error: error.message,
+        },
+      };
+    }
+  };
 }
--- src/Features/groupMessage/use-cases/getMessagesByChannelId.ts
+++ src/Features/groupMessage/use-cases/getMessagesByChannelId.ts
@@ -2,33 +2,33 @@ import { IMakeMessageDb } from "../data-access/message-db";
 import { IGroupMessage } from "../groupMessage";
 
 type props = {
-    messageDb: IMakeMessageDb["returnType"];
+  messageDb: IMakeMessageDb["returnType"];
 };
 
 type returnData = Promise<{
-    success: boolean;
-    data: IGroupMessage[] | undefined;
-    error: string;
+  success: boolean;
+  data: IGroupMessage[] | undefined;
+  error: string;
 }>;
 
 export interface IGetMessagesByChannelIdUseCase {
-    getMessagesByChannelId: (
-        messageId: string,
-        dateCreated: Date,
-        limit: number
-    ) => returnData;
+  getMessagesByChannelId: (
+    channelId: string,
+    dateCreated: Date,
+    limit: number
+  ) => returnData;
 }
 
 export default function makeGetMessagesByChannelId({ messageDb }: props) {
-    return async function getMessagesByChannelId(
-        messageId: string,
-        dateCreated: Date,
-        limit: number = 15
-    ): returnData {
-        if (!messageId) throw new Error("Message Id needs to be supplied.");
-        if (!dateCreated || Number.isNaN(dateCreated.getTime()))
-            throw new Error("Date Created needs to be supplied.");
+  return async function getMessagesByChannelId(
+    channelId: string,
+    dateCreated: Date,
+    limit: number = 15
+  ): returnData {
+    if (!channelId) throw new Error("Message Id needs to be supplied.");
+    if (!dateCreated || Number.isNaN(dateCreated.getTime()))
+      throw new Error("Date Created needs to be supplied.");
 
-        return messageDb.getMessagesByChannelId(messageId, dateCreated, limit);
-    };
+    return messageDb.getMessagesByChannelId(channelId, dateCreated, limit);
+  };
 }
