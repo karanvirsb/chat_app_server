beb5e2acdbc7026fbce66aceda6d9d679692e81b
karanvirsb <karanvirboparai123@gmail.com>
2023-01-04 19:13:15 -0500

    updated incoming messages date created

--- src/Features/groupMessage/controllers/create-message.ts
+++ src/Features/groupMessage/controllers/create-message.ts
@@ -1,47 +1,49 @@
 import {
-    IHttpRequest,
-    httpResponseType,
+  IHttpRequest,
+  httpResponseType,
 } from "../../../express-callback/index";
 import { IGroupMessage } from "../groupMessage";
 import { ICreateMessageUseCase } from "../use-cases/createMessage";
 
 interface ICreateMessageResponse extends httpResponseType {
-    body: {
-        success: boolean;
-        data: IGroupMessage | undefined;
-        error: string;
-    };
+  body: {
+    success: boolean;
+    data: IGroupMessage | undefined;
+    error: string;
+  };
 }
 
 export default function makeCreateMessageController({
-    createMessage,
+  createMessage,
 }: ICreateMessageUseCase) {
-    return async function createMessageController(
-        httpRequest: IHttpRequest
-    ): Promise<ICreateMessageResponse> {
-        const headers: { [key: string]: string } = {
-            "Content-Type": "application/json",
-        };
-        try {
-            const createdMessage = await createMessage(
-                httpRequest.body.messageInfo
-            );
-            return {
-                headers,
-                statusCode: 200,
-                body: createdMessage,
-            };
-        } catch (error: any) {
-            console.log(error);
-            return {
-                headers,
-                statusCode: 400,
-                body: {
-                    success: false,
-                    data: undefined,
-                    error: error.message,
-                },
-            };
-        }
+  return async function createMessageController(
+    httpRequest: IHttpRequest
+  ): Promise<ICreateMessageResponse> {
+    const headers: { [key: string]: string } = {
+      "Content-Type": "application/json",
     };
+    try {
+      const newMessage: IGroupMessage = httpRequest.body.messageInfo;
+      newMessage.dateCreated = new Date(
+        httpRequest.body.messageInfo.dateCreated
+      );
+      const createdMessage = await createMessage(newMessage);
+      return {
+        headers,
+        statusCode: 200,
+        body: createdMessage,
+      };
+    } catch (error: any) {
+      console.log(error);
+      return {
+        headers,
+        statusCode: 400,
+        body: {
+          success: false,
+          data: undefined,
+          error: error.message,
+        },
+      };
+    }
+  };
 }
