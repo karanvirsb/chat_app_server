1e1d1c7d529275fcd98995dd2b5f2122f44bb261
karanvirsb <karanvirboparai123@gmail.com>
2023-02-18 10:47:35 -0500

    updated error checks

--- src/Features/groupUsers/groupUsers.ts
+++ src/Features/groupUsers/groupUsers.ts
@@ -12,9 +12,12 @@ export default function makeGroupUser() {
     roles,
     lastChecked,
   }: IGroupUser) {
-    if (groupId === null) throw new Error("Group Id must be string.");
-    if (userId === null) throw new Error("UserId must be string.");
-    if (roles === null) throw new Error("Roles must be array.");
+    if (groupId === null || groupId.length <= 0)
+      throw new Error("Group Id must be string.");
+    if (userId === null || userId.length <= 0)
+      throw new Error("UserId must be string.");
+    if (roles === null || roles.length <= 0)
+      throw new Error("Roles must be array.");
     if (lastChecked === null || Number.isNaN(lastChecked.getTime()))
       throw new Error("LastChecked must be a real date.");
 
