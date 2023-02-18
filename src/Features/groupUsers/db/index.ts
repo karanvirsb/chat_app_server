ff41a373a0c8dc3997b61d82122f158a3b57242b
karanvirsb <karanvirboparai123@gmail.com>
2023-02-18 11:10:04 -0500

    created db

---
+++ src/Features/groupUsers/db/index.ts
@@ -0,0 +1,22 @@
+import { makeDb } from "../data-access";
+import dotenv from "dotenv";
+dotenv.config();
+
+export default async function setupGroupDb() {
+  console.log("setting up group users database...");
+  // database will be created if it doesnt exist already
+  try {
+    const db = await makeDb();
+    const result = await db.query(`
+      CREATE TABLE IF NOT EXISTS "groupUsers" (
+        "gId" VARCHAR(100) REFERENCES groupt("groupId") ON DELETE CASCADE ON UPDATE CASCADE,
+        "uId" VARCHAR(100) REFERENCES usert("userId") ON UPDATE CASCADE ON DELETE CASCADE,
+        roles TEXT [],
+        PRIMARY KEY ("gId", "uId")
+      );
+    `);
+    console.log("Group users Database set up complete...");
+  } catch (err) {
+    console.log("Group users DB ERROR:", err);
+  }
+}
