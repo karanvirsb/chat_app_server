ed735e66f4c5fd47453fc76e109eccfa62ad8230
karanvirsb <karanvirboparai123@gmail.com>
2023-02-18 11:17:44 -0500

    added databse access

--- src/Features/groupUsers/slice/createGroupUser.ts
+++ src/Features/groupUsers/slice/createGroupUser.ts
@@ -1,3 +1,46 @@
+import { IGroupUsersDb } from "../data-access";
+import { IGroupUser } from "../groupUsers";
+
 export function makeCreateGroupUseCase() {}
 
-export function makeCreateGroupDBAccess() {}
+type makeCreateGroupDBAccessProps = {
+  makeDb: IGroupUsersDb["makeDb"];
+};
+
+export function makeCreateGroupDBAccess({
+  makeDb,
+}: makeCreateGroupDBAccessProps) {
+  return async function createGroupDb({
+    groupId,
+    userId,
+    lastChecked,
+    roles,
+  }: IGroupUser) {
+    const db = await makeDb(); // creating db access
+    try {
+      const q = `INSERT INTO "groupUsers" values('${groupId}', '${userId}', '{2000}', to_timestamp(${lastChecked.getTime()}/1000),) RETURNING *;`;
+      const result = await db.query(q);
+
+      if (result.rows.length >= 1) {
+        const groupUser = result.rows[0];
+        return { success: true, data: groupUser, error: "" };
+      } else {
+        return {
+          success: true,
+          data: undefined,
+          error: "Could not insert group user",
+        };
+      }
+    } catch (error) {
+      console.log("🚀 ~ file: createGroupUser.ts:35 ~ error", error);
+
+      return {
+        success: false,
+        data: undefined,
+        error: error + "",
+      };
+    } finally {
+      db.release();
+    }
+  };
+}
