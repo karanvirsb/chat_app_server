06b1066e413e41a93a7db344e8f3db2d9b3d7668
karanvirsb <karanvirboparai123@gmail.com>
2023-02-18 10:17:19 -0500

    added last active

--- src/Features/group/group.ts
+++ src/Features/group/group.ts
@@ -2,61 +2,60 @@ import { IId } from "../../Utilities/id";
 import { IInviteCodeGenerator } from "../../Utilities/inviteCodeGenerator";
 
 type props = {
-    Id: IId;
-    sanitizeText: (text: string) => string;
-    inviteCodeGenerator: IInviteCodeGenerator;
+  Id: IId;
+  sanitizeText: (text: string) => string;
+  inviteCodeGenerator: IInviteCodeGenerator;
 };
 
 export interface IGroup {
-    groupName: string;
-    groupId: string;
-    inviteCode: string;
-    dateCreated: Date;
+  groupName: string;
+  groupId: string;
+  inviteCode: string;
+  dateCreated: Date;
+  // lastActive: Date
 }
 
 export default function buildGroup({
-    Id,
-    sanitizeText,
-    inviteCodeGenerator,
+  Id,
+  sanitizeText,
+  inviteCodeGenerator,
 }: props) {
-    return function makeGroup({
-        groupName,
-        groupId = Id.makeId(),
-        inviteCode = inviteCodeGenerator.makeInviteCode(),
-        dateCreated = new Date(),
-    }: IGroup) {
-        const regex = /'/g;
-        let sanitizedGroupName = sanitizeText(groupName);
+  return function makeGroup({
+    groupName,
+    groupId = Id.makeId(),
+    inviteCode = inviteCodeGenerator.makeInviteCode(),
+    dateCreated = new Date(),
+  }: IGroup) {
+    const regex = /'/g;
+    let sanitizedGroupName = sanitizeText(groupName);
 
-        if (sanitizedGroupName.length < 1) {
-            throw new Error("Group name must contain valid characters");
-        }
+    if (sanitizedGroupName.length < 1) {
+      throw new Error("Group name must contain valid characters");
+    }
 
-        if (sanitizedGroupName.length < 3 || sanitizedGroupName.length > 50) {
-            throw new Error(
-                "Group name must be between 3 and 50 characters long"
-            );
-        }
+    if (sanitizedGroupName.length < 3 || sanitizedGroupName.length > 50) {
+      throw new Error("Group name must be between 3 and 50 characters long");
+    }
 
-        if (!groupId) {
-            throw new Error("Group requires an Id");
-        }
+    if (!groupId) {
+      throw new Error("Group requires an Id");
+    }
 
-        if (!inviteCode) {
-            throw new Error("Group requires an invite code");
-        }
+    if (!inviteCode) {
+      throw new Error("Group requires an invite code");
+    }
 
-        if (!dateCreated || Number.isNaN(dateCreated.getTime()))
-            throw new Error("Date Created needs to be supplied.");
+    if (!dateCreated || Number.isNaN(dateCreated.getTime()))
+      throw new Error("Date Created needs to be supplied.");
 
-        // replace any ' with a '' to escape
-        const newGroupName = sanitizedGroupName.replace(regex, "''");
+    // replace any ' with a '' to escape
+    const newGroupName = sanitizedGroupName.replace(regex, "''");
 
-        return Object.freeze({
-            getGroupName: () => newGroupName,
-            getGroupId: () => groupId,
-            getInviteCode: () => inviteCode,
-            getDateCreated: () => dateCreated,
-        });
-    };
+    return Object.freeze({
+      getGroupName: () => newGroupName,
+      getGroupId: () => groupId,
+      getInviteCode: () => inviteCode,
+      getDateCreated: () => dateCreated,
+    });
+  };
 }
--- src/Features/privateChannel/privateChannel.ts
+++ src/Features/privateChannel/privateChannel.ts
@@ -1,71 +1,67 @@
 import { IId } from "../../Utilities/id";
 
 export interface IPrivateChannel {
-    channelId: string;
-    channelName: string;
-    dateCreated: Date;
-    userId: string;
-    friendsId: string;
-    lastActive: Date;
+  channelId: string;
+  channelName: string;
+  dateCreated: Date;
+  userId: string;
+  friendsId: string;
+  lastActive: Date;
+  // lastRead: Date;
 }
 
 type props = {
-    Id: IId;
-    sanitizeText: (text: string) => string;
+  Id: IId;
+  sanitizeText: (text: string) => string;
 };
 
 export default function buildPrivateChannel({ Id, sanitizeText }: props) {
-    return function makePrivateChannel({
-        channelId = Id.makeId(),
-        channelName,
-        userId,
-        friendsId,
-        dateCreated = new Date(),
-        lastActive = new Date(),
-    }: IPrivateChannel) {
-        const sanitizedChannelName = sanitizeText(channelName);
+  return function makePrivateChannel({
+    channelId = Id.makeId(),
+    channelName,
+    userId,
+    friendsId,
+    dateCreated = new Date(),
+    lastActive = new Date(),
+  }: IPrivateChannel) {
+    const sanitizedChannelName = sanitizeText(channelName);
 
-        if (sanitizedChannelName.length <= 1) {
-            throw new Error("Channel name should contain valid characters");
-        }
+    if (sanitizedChannelName.length <= 1) {
+      throw new Error("Channel name should contain valid characters");
+    }
 
-        if (
-            sanitizedChannelName.length < 3 ||
-            sanitizedChannelName.length > 50
-        ) {
-            throw new Error(
-                "Channel name should be between 3 to 50 characters long"
-            );
-        }
+    if (sanitizedChannelName.length < 3 || sanitizedChannelName.length > 50) {
+      throw new Error("Channel name should be between 3 to 50 characters long");
+    }
 
-        if (!userId) {
-            throw new Error("User Id needs to be supplied");
-        }
+    if (!userId) {
+      throw new Error("User Id needs to be supplied");
+    }
 
-        if (!friendsId) throw new Error("Friends Id needs to be supplied");
+    if (!friendsId) throw new Error("Friends Id needs to be supplied");
 
-        if (!dateCreated) {
-            throw new Error("Date needs to be supplied");
-        }
+    if (!dateCreated) {
+      throw new Error("Date needs to be supplied");
+    }
 
-        if (!channelId) {
-            throw new Error("Channel Id needs to be supplied");
-        }
+    if (!channelId) {
+      throw new Error("Channel Id needs to be supplied");
+    }
 
-        if (!lastActive || Number.isNaN(lastActive.getTime())) {
-            throw new Error("Last active needs to be supplied");
-        }
+    if (!lastActive || Number.isNaN(lastActive.getTime())) {
+      throw new Error("Last active needs to be supplied");
+    }
 
-        // replace any ' with a '' to escape
-        const newChannelName = sanitizedChannelName.replace(/'/g, "''");
+    // replace any ' with a '' to escape
+    const newChannelName = sanitizedChannelName.replace(/'/g, "''");
 
-        return Object.freeze({
-            getChannelId: () => channelId,
-            getChannelName: () => newChannelName,
-            getUserId: () => userId,
-            getFriendsId: () => friendsId,
-            getDateCreated: () => dateCreated,
-            getLastActive: () => lastActive,
-        });
-    };
+    return Object.freeze({
+      getChannelId: () => channelId,
+      getChannelName: () => newChannelName,
+      getUserId: () => userId,
+      getFriendsId: () => friendsId,
+      getDateCreated: () => dateCreated,
+      getLastActive: () => lastActive,
+    });
+  };
 }
