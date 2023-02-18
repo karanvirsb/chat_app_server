2e9e56948a73355fc4deafba1475742e30ffc14d
karanvirsb <karanvirboparai123@gmail.com>
2023-02-18 10:48:16 -0500

    added test for groupid

--- src/Features/groupUsers/index.spec.ts
+++ src/Features/groupUsers/index.spec.ts
@@ -13,7 +13,13 @@ describe("Testing group user", () => {
     expect(user.getUserId()).toBe(fakeGroupUser.userId);
     expect(user.getGroupId()).toBe(fakeGroupUser.groupId);
   });
-  it("ERROR: groupid needs to exist", () => {});
+  it("ERROR: groupid needs to exist", () => {
+    const userWithoutGroupId = fakeGroupUser;
+    userWithoutGroupId.groupId = "";
+    expect(() => buildGroupUser(userWithoutGroupId)).toThrow(
+      "Group Id must be string."
+    );
+  });
   it("ERROR: userid needs to exist", () => {});
   it("ERROR: roles needs to exist", () => {});
 });
