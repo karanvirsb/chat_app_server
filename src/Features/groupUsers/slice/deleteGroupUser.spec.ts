8925cee5d1228d97c1de12366778bdef8d8f8189
karanvirsb <karanvirboparai123@gmail.com>
2023-02-21 12:27:24 -0500

    created db test for delete group user

---
+++ src/Features/groupUsers/slice/deleteGroupUser.spec.ts
@@ -0,0 +1,33 @@
+import { makeDeleteGroupUserDBA } from "./deleteGroupUser";
+import makeDb from "../../../../__test__/fixures/db";
+import groupTests from "../../../../__test__/functions/group";
+import userTests from "../../../../__test__/functions/user";
+
+describe("Testing deleting group user DB", () => {
+  const deleteGroupUserDBA = makeDeleteGroupUserDBA({ makeDb });
+  beforeAll(async () => {
+    let testUser = await userTests.addTestUserToDB({ userId: "123" });
+    let testGroup = await groupTests.createTestGroup({
+      groupId: "123",
+      userId: "123",
+    });
+  });
+
+  afterAll(async () => {
+    let testUser = await userTests.deleteTestUser({ userId: "123" });
+    let testGroup = await groupTests.deleteTestGroup({
+      groupId: "123",
+      userId: "123",
+    });
+  });
+  it("Create user successfully", async () => {
+    const deleteGroupUser = await deleteGroupUserDBA({
+      groupId: "123",
+      userId: "123",
+    });
+    console.log(deleteGroupUser);
+    expect(deleteGroupUser.data?.gId).toBe("123");
+
+    expect(deleteGroupUser.data?.uId).toBe("123");
+  });
+});
