c80704f989fdb73fb388a5f648e68d5403fffffe
karanvirsb <karanvirboparai123@gmail.com>
2023-02-21 11:15:42 -0500

    createed db access for removing user from group

---
+++ src/Features/groupUsers/slice/deleteGroupUser.ts
@@ -0,0 +1,44 @@
+import { IGroupUsersDb } from "../data-access";
+import { IGroupUser } from "../groupUsers";
+
+export type deleteGroupUserDBAProps = {
+  makeDb: IGroupUsersDb["makeDb"];
+};
+
+export function deleteGroupUserDBA({ makeDb }: deleteGroupUserDBAProps) {
+  return async function deleteGroupUserDB({
+    groupId,
+    userId,
+  }: {
+    groupId: string;
+    userId: string;
+  }) {
+    const db = await makeDb();
+
+    try {
+      const query = `
+      DELETE FROM "groupUsers" 
+      WHERE "gId" = '${groupId}' AND "uId" = '${userId}' 
+      RETURNING *`;
+
+      const result = await db.query(query);
+
+      if (result.rows.length >= 1) {
+        const groupUser: IGroupUser = result.rows[0];
+        return {
+          success: true,
+          data: groupUser,
+          error: "",
+        };
+      }
+    } catch (error) {
+      return {
+        success: true,
+        data: [],
+        error: "Could not remove the user from the group.",
+      };
+    } finally {
+      db.release();
+    }
+  };
+}
