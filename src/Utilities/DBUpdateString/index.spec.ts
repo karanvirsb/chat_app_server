23ff8ec8d7dd005a873dec2dcaf7a45ee87f9687
karanvirsb <karanvirboparai123@gmail.com>
2023-02-24 20:44:23 -0500

    added test for boolean

--- src/Utilities/DBUpdateString/index.spec.ts
+++ src/Utilities/DBUpdateString/index.spec.ts
@@ -1,11 +1,14 @@
-import { StringToDBString } from ".";
+import { BooleanToDBBoolean, StringToDBString } from ".";
 
 describe("Testing out DBUpdateString", () => {
   it("Testing out String", () => {
     const result = StringToDBString("name", "john");
     expect(result).toBe("'name' = \"john\"");
   });
-  it.skip("Testing out Boolean", () => {});
+  it("Testing out Boolean", () => {
+    const result = BooleanToDBBoolean("name", "true");
+    expect(result).toBe("'name' = TRUE");
+  });
   it.skip("Testing out Number", () => {});
   it.skip("Testing out Date", () => {});
   it.skip("Testing out Null", () => {});
