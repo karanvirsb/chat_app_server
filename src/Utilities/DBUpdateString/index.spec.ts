d30f2cadecb32fd198c0decfa9fcef4494c14d1c
karanvirsb <karanvirboparai123@gmail.com>
2023-02-24 20:42:05 -0500

    added test for string and added callbacks

--- src/Utilities/DBUpdateString/index.spec.ts
+++ src/Utilities/DBUpdateString/index.spec.ts
@@ -1,8 +1,13 @@
+import { StringToDBString } from ".";
+
 describe("Testing out DBUpdateString", () => {
-  it("Testing out String");
-  it("Testing out Boolean");
-  it("Testing out Number");
-  it("Testing out Date");
-  it("Testing out Null");
-  it("Testing out Array");
+  it("Testing out String", () => {
+    const result = StringToDBString("name", "john");
+    expect(result).toBe("'name' = \"john\"");
+  });
+  it.skip("Testing out Boolean", () => {});
+  it.skip("Testing out Number", () => {});
+  it.skip("Testing out Date", () => {});
+  it.skip("Testing out Null", () => {});
+  it.skip("Testing out Array", () => {});
 });
