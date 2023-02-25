443b7e6e2d232c6e288ab2f248785b6c70d4c78a
karanvirsb <karanvirboparai123@gmail.com>
2023-02-24 20:48:48 -0500

    added test for date

--- src/Utilities/DBUpdateString/index.spec.ts
+++ src/Utilities/DBUpdateString/index.spec.ts
@@ -1,4 +1,9 @@
-import { BooleanToDBBoolean, NumberToDBNumber, StringToDBString } from ".";
+import {
+  BooleanToDBBoolean,
+  DateToDBDate,
+  NumberToDBNumber,
+  StringToDBString,
+} from ".";
 
 describe("Testing out DBUpdateString", () => {
   it("Testing out String", () => {
@@ -13,7 +18,11 @@ describe("Testing out DBUpdateString", () => {
     const res = NumberToDBNumber("num", 15);
     expect(res).toBe("'num' = 15");
   });
-  it.skip("Testing out Date", () => {});
+  it("Testing out Date", () => {
+    const date = new Date();
+    const res = DateToDBDate("date", date);
+    expect(res).toBe(`'date' = to_timestamp(${date.getTime()}/1000)`);
+  });
   it.skip("Testing out Null", () => {});
   it.skip("Testing out Array", () => {});
 });
