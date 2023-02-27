dec2e298cce689aab5d05994d7a976bd7a89c1c4
karanvirsb <karanvirboparai123@gmail.com>
2023-02-27 10:30:11 -0500

    added boilerplate

--- src/Features/groupUsers/slice/updateGroupUser.spec.ts
+++ src/Features/groupUsers/slice/updateGroupUser.spec.ts
@@ -1,4 +1,35 @@
+import groupTests from "../../../../__test__/functions/group";
+import groupUserTests from "../../../../__test__/functions/groupUser";
+import userTests from "../../../../__test__/functions/user";
+import DBUpdateStr from "../../../Utilities/DBUpdateString";
+import { makeDb } from "../data-access";
+import { makeUpdateGroupUserDBA } from "./updateGroupUser";
+
 describe("Testing update group user DB Access", () => {
+  const updateGroupUserDBA = makeUpdateGroupUserDBA({ makeDb, DBUpdateStr });
+  beforeAll(async () => {
+    let testUser = await userTests.addTestUserToDB({ userId: "123" });
+    let testGroup = await groupTests.createTestGroup({
+      groupId: "123",
+      userId: "123",
+    });
+    let testGroupUser = await groupUserTests.createGroupUserTest({
+      groupId: "123",
+      userId: "123",
+    });
+  });
+
+  afterAll(async () => {
+    let testUser = await userTests.deleteTestUser({ userId: "123" });
+    let testGroup = await groupTests.deleteTestGroup({
+      groupId: "123",
+      userId: "123",
+    });
+    let testGroupUser = await groupUserTests.deleteGroupUserTest({
+      groupId: "123",
+      userId: "123",
+    });
+  });
   it("Successfully updating group roles", async () => {});
   it("Successfully updating lastChecked");
 });
