a1ffe9bd5a36f5411b9096d85a0c8161dfd03033
karanvirsb <karanvirboparai123@gmail.com>
2023-02-27 10:45:20 -0500

    updated tests

--- src/Utilities/DBUpdateString/index.spec.ts
+++ src/Utilities/DBUpdateString/index.spec.ts
@@ -10,28 +10,28 @@ import DBUpdateStr, {
 describe("Testing out DBUpdateString", () => {
   it("Testing out String", () => {
     const result = StringToDBString("name", "john");
-    expect(result).toBe("'name' = \"john\"");
+    expect(result).toBe(`"name" = 'john'`);
   });
   it("Testing out Boolean", () => {
     const result = BooleanToDBBoolean("name", true);
-    expect(result).toBe("'name' = TRUE");
+    expect(result).toBe(`"name" = TRUE`);
   });
   it("Testing out Number", () => {
     const res = NumberToDBNumber("num", 15);
-    expect(res).toBe("'num' = 15");
+    expect(res).toBe(`"num" = 15`);
   });
   it("Testing out Date", () => {
     const date = new Date();
     const res = DateToDBDate("date", date);
-    expect(res).toBe(`'date' = to_timestamp(${date.getTime()}/1000)`);
+    expect(res).toBe(`"date" = to_timestamp(${date.getTime()}/1000)`);
   });
   it("Testing out Null", () => {
     const res = NullToDBNull("date");
-    expect(res).toBe(`'date' = NULL`);
+    expect(res).toBe(`"date" = NULL`);
   });
   it("Testing out Array", () => {
     const res = ArrayToDBArray("random", [12, 14, true, "nice", ["str", 12]]);
-    expect(res).toBe(`'random' = '{12, 14, TRUE, "nice", '{"str", 12}'}'`);
+    expect(res).toBe(`"random" = '{12, 14, TRUE, "nice", '{"str", 12}'}'`);
   });
 
   it("Testing out full function", () => {
@@ -45,7 +45,7 @@ describe("Testing out DBUpdateString", () => {
     };
     const res = DBUpdateStr(obj);
     expect(res).toBe(
-      `'name' = "john", 'boo' = TRUE, 'num' = 15, 'date' = to_timestamp(${obj.date.getTime()}/1000), 'email' = NULL, 'random' = '{12, 14, TRUE, "nice", '{"str", 12}'}'`
+      `"name" = "john", "boo" = TRUE, "num" = 15, "date" = to_timestamp(${obj.date.getTime()}/1000), "email" = NULL, "random" = '{12, 14, TRUE, "nice", '{"str", 12}'}'`
     );
   });
 });
