0cde59905aca6ecab6a891a93abad3628f7e9d8c
karanvirsb <karanvirboparai123@gmail.com>
2023-03-01 12:08:17 -0500

    updated added zod error

--- src/Features/groupUsers/groupUsers.ts
+++ src/Features/groupUsers/groupUsers.ts
@@ -1,9 +1,9 @@
-import { z } from "zod";
+import { ZodError, z } from "zod";
 export const IGroupUserSchema = z.object({
-  gId: z.string(),
+  gId: z.string().min(21),
   lastChecked: z.date(),
   roles: z.array(z.string()),
-  uId: z.string(),
+  uId: z.string().min(21),
 });
 
 export type IGroupUser = z.infer<typeof IGroupUserSchema>;
@@ -15,13 +15,16 @@ export default function makeGroupUser() {
     roles,
     lastChecked,
   }: IGroupUser) {
-    if (gId === null || gId.length <= 0)
-      throw new Error("Group Id must be string.");
-    if (uId === null || uId.length <= 0) throw new Error("uId must be string.");
-    if (roles === null || roles.length <= 0)
-      throw new Error("Roles must be an array.");
-    if (lastChecked === null || Number.isNaN(lastChecked.getTime()))
-      throw new Error("LastChecked must be a real date.");
+    // if (gId === null || gId.length <= 0)
+    //   throw new Error("Group Id must be string.");
+    // if (uId === null || uId.length <= 0) throw new Error("uId must be string.");
+    // if (roles === null || roles.length <= 0)
+    //   throw new Error("Roles must be an array.");
+    // if (lastChecked === null || Number.isNaN(lastChecked.getTime()))
+    //   throw new Error("LastChecked must be a real date.");
+
+    const result = IGroupUserSchema.safeParse({ gId, uId, roles, lastChecked });
+    if (!result.success) throw new ZodError(result.error.errors);
 
     return Object.freeze({
       getgId: () => gId,
