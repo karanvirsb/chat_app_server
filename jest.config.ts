475a397cc0b1e042c4bc7be4952af1860fa6e2d8
karanvirsb <karanvirboparai123@gmail.com>
2023-03-01 11:44:15 -0500

    updated config

--- jest.config.ts
+++ jest.config.ts
@@ -2,14 +2,25 @@ import type { Config } from "@jest/types";
 
 // Sync object
 const config: Config.InitialOptions = {
-    verbose: true,
-    testEnvironment: "node",
-    preset: "ts-jest",
-    transform: {
-        "^.+\\.(t|j)sx?$": "@swc/jest",
-        // "^.+\\.(ts|tsx)?$": "ts-jest",
-        // "^.+\\.(js|jsx)$": "babel-jest",
-    },
-    transformIgnorePatterns: ["/server/node_modules"],
+  verbose: true,
+  testEnvironment: "node",
+  preset: "ts-jest",
+  transform: {
+    "^.+\\.(t|j)sx?$": "@swc/jest",
+    // "^.+\\.(ts|tsx)?$": "ts-jest",
+    // "^.+\\.(js|jsx)$": "babel-jest",
+  },
+  transformIgnorePatterns: ["/server/node_modules"],
+  moduleFileExtensions: [
+    "ts",
+    "tsx",
+    "d.ts",
+    "json",
+    "js",
+    "mjs",
+    "cjs",
+    "jsx",
+    "node",
+  ],
 };
 export default config;
