a9398b385dea82c7c4d60197b6fb899773aea8e4
karanvirsb <karanvirboparai123@gmail.com>
2023-03-01 11:04:51 -0500

    created file for update group user types

---
+++ src/Features/groupUsers/types/updateGroupUser.d.ts
@@ -0,0 +1,51 @@
+import { ZodError, z } from "zod";
+import { IGroupUsersDb } from "../data-access";
+import { DBUpdateStr } from "../../../Utilities/DBUpdateString";
+import { httpResponseType } from "../../../express-callback";
+
+import { IGroupUser, IGroupUserSchema } from "../groupUsers";
+
+export const updateGroupUserProps = z.object({
+  groupId: z.string(),
+  userId: z.string(),
+  updates: IGroupUserSchema.partial().omit({ gId: true, uId: true }),
+});
+
+export type updateGroupUserProps = z.infer<typeof updateGroupUserProps>;
+
+export type makeUpdateGroupUserControllerDeps = {
+  updateGroupUserUC: ({
+    groupId,
+    userId,
+    updates,
+  }: updateGroupUserProps) => Promise<{
+    success: boolean;
+    data: IGroupUser | undefined;
+    error: string;
+  }>;
+};
+
+export interface IUpdateGroupUserC extends httpResponseType {
+  body: {
+    success: boolean;
+    data: IGroupUser | undefined;
+    error: string;
+  };
+}
+
+export type makeUpdateGroupUserUCDeps = {
+  updateGroupUserDBA: ({
+    groupId,
+    userId,
+    updates,
+  }: updateGroupUserProps) => Promise<{
+    success: boolean;
+    data: IGroupUser | undefined;
+    error: string;
+  }>;
+};
+
+export type makeUpdateGroupUserDBADeps = {
+  makeDb: IGroupUsersDb["makeDb"];
+  DBUpdateStr: DBUpdateStr;
+};
