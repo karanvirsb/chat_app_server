c42e92bf7cb4890a485f71eb02f4e935ebaaea64
karanvirsb <karanvirboparai123@gmail.com>
2023-03-08 14:50:03 -0500

    exports update group user

--- src/Features/groupUsers/slice/index.ts
+++ src/Features/groupUsers/slice/index.ts
@@ -8,6 +8,12 @@ import {
   makeDeleteGroupUserDBA,
   makeDeleteGroupUserUC,
 } from "./deleteGroupUser";
+import {
+  makeUpdateGroupUserController,
+  makeUpdateGroupUserDBA,
+  makeUpdateGroupUserUC,
+} from "./updateGroupUser";
+import DBUpdateStr from "../../../Utilities/DBUpdateString";
 
 // CREATE GROUP USER
 export const createGroupUserDBA = makeCreateGroupDBAccess({ makeDb });
@@ -20,4 +26,14 @@ export const deleteGroupUserDBA = makeDeleteGroupUserDBA({ makeDb });
 export const deleteGroupUserUC = makeDeleteGroupUserUC({ deleteGroupUserDBA });
 export const deleteGroupUserController = makeDeleteGroupUserController({
   deleteGroupUserUC,
+});
+
+// UPDATE GROUP USER
+export const updateGroupUserDBA = makeUpdateGroupUserDBA({
+  makeDb,
+  DBUpdateStr,
+});
+export const updateGroupUserUC = makeUpdateGroupUserUC({ updateGroupUserDBA });
+export const updateGroupUserController = makeUpdateGroupUserController({
+  updateGroupUserUC,
 });
