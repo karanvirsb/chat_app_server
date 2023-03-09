a687018a9f68074a264b5c317a6d6d9b42eaabe7
karanvirsb <karanvirboparai123@gmail.com>
2023-03-08 21:50:42 -0500

    removed add method

--- src/Features/user/controllers/index.ts
+++ src/Features/user/controllers/index.ts
@@ -1,20 +1,18 @@
-import { getUser, addUser, deleteUser, editUser } from "../use-cases";
+import { getUser, deleteUser, editUser } from "../use-cases";
 import makeDeleteUser from "../DeleteUser/deleteUserController";
 import makeEditUser from "./edit-user";
 import makeGetUser from "./get-user";
 import makePostUser from "../AddUser/addUserController";
 
 const getAnUser = makeGetUser({ getUser });
-const addAnUser = makePostUser({ addUser });
 const deleteAnUser = makeDeleteUser({ deleteUser });
 const editAnUser = makeEditUser({ editUser });
 
 const userController = Object.freeze({
   getAnUser,
-  addAnUser,
   deleteAnUser,
   editAnUser,
 });
 export default userController;
 
-export { getAnUser, addAnUser, deleteAnUser, editAnUser };
+export { getAnUser, deleteAnUser, editAnUser };
