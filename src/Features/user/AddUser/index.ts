e9b1c540a25d37468fe65d6d9a06a8fc7978910c
karanvirsb <karanvirboparai123@gmail.com>
2023-03-08 21:14:33 -0500

    added use case and controller

---
+++ src/Features/user/AddUser/index.ts
@@ -0,0 +1,13 @@
+import makePostUser from "../AddUser/post-user";
+import makeAddUser from "../AddUser/addUser";
+import { moderateName } from "../../../Utilities/moderateText";
+import usersDb from "../data-access";
+
+const handleModeration = async (name: string) => {
+  return await moderateName(name);
+};
+
+export const addUserUC = makeAddUser({ usersDb, handleModeration });
+export const addUserController = makePostUser({ addUser: addUserUC });
+
+export type handleModerationType = (name: string) => Promise<number | boolean>;
