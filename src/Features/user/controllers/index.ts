6bda54926995f3e5880d7e0f69e7d2649511bfde
karanvirsb <karanvirboparai123@gmail.com>
2023-03-08 21:55:06 -0500

    removed unused imports

--- src/Features/user/controllers/index.ts
+++ src/Features/user/controllers/index.ts
@@ -1,18 +1,14 @@
-import { getUser, deleteUser, editUser } from "../use-cases";
-import makeDeleteUser from "../DeleteUser/deleteUserController";
+import { getUser, editUser } from "../use-cases";
 import makeEditUser from "./edit-user";
 import makeGetUser from "./get-user";
-import makePostUser from "../AddUser/addUserController";
 
 const getAnUser = makeGetUser({ getUser });
-const deleteAnUser = makeDeleteUser({ deleteUser });
 const editAnUser = makeEditUser({ editUser });
 
 const userController = Object.freeze({
   getAnUser,
-  deleteAnUser,
   editAnUser,
 });
 export default userController;
 
-export { getAnUser, deleteAnUser, editAnUser };
+export { getAnUser, editAnUser };
